211a00cf756da7a145b40dbdbaf4d0f77a04e917
Joseph Platta <jwplatta@gmail.com>
2025-01-23 22:20:22 -0600

    add instrument and asset classes

--- models/instrument.rb
+++ models/instrument.rb
@@ -1,20 +1,86 @@
-class Instrument
-  attr_reader :asset_type, :cusip, :symbol, :description, :net_change, :type, :put_call, :underlying_symbol
+class Asset
+  attr_reader :asset_type, :status, :symbol, :instrument_id, :closing_price, :type, :description,
+    :active_contract, :expiration_date, :last_trading_date, :multiplier, :future_type
 
   def self.build(data)
     new(
-      asset_type: data[:assetType],
-      cusip: data[:cusip],
-      symbol: data[:symbol],
-      description: data[:description],
-      net_change: data[:netChange],
-      type: data[:type],
-      put_call: data[:putCall],
-      underlying_symbol: data[:underlyingSymbol]
+      asset_type: data.fetch(:assetType, nil),
+      status: data.fetch(:status, nil),
+      symbol: data.fetch(:symbol, nil),
+      instrument_id: data.fetch(:instrumentId, nil),
+      closing_price: data.fetch(:closingPrice, nil),
+      type: data.fetch(:type, nil),
+      description: data.fetch(:description, nil),
+      active_contract: data.fetch(:activeContract, nil),
+      expiration_date: data.fetch(:expirationDate, nil),
+      last_trading_date: data.fetch(:lastTradingDate, nil),
+      multiplier: data.fetch(:multiplier, nil),
+      future_type: data.fetch(:futureType, nil)
     )
   end
 
-  def initialize(asset_type:, cusip:, symbol:, description:, net_change:, type:, put_call:, underlying_symbol:)
+  def initialize(asset_type:, status:, symbol:, instrument_id:, closing_price:, type:, description:, active_contract:, expiration_date:, last_trading_date:, multiplier:, future_type:)
+    @asset_type = asset_type
+    @status = status
+    @symbol = symbol
+    @instrument_id = instrument_id
+    @closing_price = closing_price
+    @type = type
+    @description = description
+    @active_contract = active_contract
+    @expiration_date = expiration_date
+    @last_trading_date = last_trading_date
+    @multiplier = multiplier
+    @future_type = future_type
+  end
+end
+
+class OptionDeliverable
+  attr_reader :root_symbol, :symbol, :deliverable_units, :deliverable_number, :strike_percent, :deliverable
+
+  def self.build(data)
+    new(
+      root_symbol: data.fetch(:rootSymbol, nil),
+      symbol: data.fetch(:symbol, nil),
+      strike_percent: data.fetch(:strikePercent, nil),
+      deliverable_number: data.fetch(:deliverableNumber, nil),
+      deliverable_units: data.fetch(:deliverableUnits),
+      deliverable: data.fetch(:deliverable, nil).then { |d| d.nil? ? nil : Instrument.build(d) }
+    )
+  end
+
+  def initialize(root_symbol:, symbol:, deliverable_units:, deliverable_number:, strike_percent:, deliverable:)
+    @root_symbol = root_symbol
+    @symbol = symbol
+    @deliverable_units = deliverable_units
+    @deliverable_number = deliverable_number
+    @strike_percent = strike_percent
+    @deliverable = deliverable
+  end
+end
+
+class Instrument
+  attr_reader :asset_type, :cusip, :symbol, :description, :net_change, :type, :put_call,
+    :underlying_symbol, :status, :instrument_id, :closing_price, :option_deliverables
+
+  def self.build(data)
+    new(
+      asset_type: data.fetch(:assetType),
+      symbol: data.fetch(:symbol, nil),
+      description: data.fetch(:description),
+      cusip: data.fetch(:cusip, nil),
+      net_change: data.fetch(:netChange, nil),
+      type: data.fetch(:type, nil),
+      put_call: data.fetch(:putCall, nil),
+      underlying_symbol: data.fetch(:underlyingSymbol, nil),
+      status: data.fetch(:status, nil),
+      instrument_id: data.fetch(:instrumentId, nil),
+      closing_price: data.fetch(:closingPrice, nil),
+      option_deliverables: data.fetch(:optionDeliverables, []).map { |d| OptionDeliverable.build(d) }
+    )
+  end
+
+  def initialize(asset_type:, cusip:, symbol:, description:, net_change:, type:, put_call:, underlying_symbol:, status:, instrument_id:, closing_price:, option_deliverables: [])
     @asset_type = asset_type
     @cusip = cusip
     @symbol = symbol
@@ -23,5 +89,9 @@ class Instrument
     @type = type
     @put_call = put_call
     @underlying_symbol = underlying_symbol
+    @status = status
+    @instrument_id = instrument_id
+    @closing_price = closing_price
+    @option_deliverables = option_deliverables
   end
 end
