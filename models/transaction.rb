92a52862cdb1c103a957609a999decef57ec30ce
Joseph Platta <jwplatta@gmail.com>
2025-01-23 22:20:59 -0600

    add a transfer item class

--- models/transaction.rb
+++ models/transaction.rb
@@ -1,3 +1,26 @@
+require_relative "instrument"
+
+class TransferItem
+  class << self
+    def build(data)
+      TransferItem.new(
+        instrument: Instrument.build(data.fetch(:instrument)),
+        amount: data.fetch(:amount),
+        cost: data.fetch(:cost),
+        fee_type: data.fetch(:feeType, nil),
+      )
+    end
+  end
+  def initialize(instrument:, amount:, cost:, fee_type:)
+    @instrument = instrument
+    @amount = amount
+    @cost = cost
+    @fee_type = fee_type
+  end
+
+  attr_reader :instrument, :amount, :cost, :fee_type
+end
+
 class Transaction
   class << self
     def build(data)
@@ -9,16 +32,16 @@ class Transaction
         status: data.fetch(:status),
         sub_account: data.fetch(:subAccount),
         trade_date: data.fetch(:tradeDate),
-        position_id: data.fetch(:positionId),
-        order_id: data.fetch(:orderId),
+        position_id: data.fetch(:positionId, nil),
+        order_id: data.fetch(:orderId, nil),
         net_amount: data.fetch(:netAmount),
-        transfer_items: transfer_items
+        transfer_items: data.fetch(:transferItems).map { |ti| TransferItem.build(ti) }
       )
     end
   end
 
   def initialize(activity_id:, time:, account_number:, type:, status:, sub_account:, trade_date:, position_id:, order_id:, net_amount:, transfer_items: [])
-    @actibity_id = activity_id
+    @activity_id = activity_id
     @time = time
     @account_number = account_number
     @type = type
