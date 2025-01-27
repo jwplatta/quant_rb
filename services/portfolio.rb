b804c6774e95816142dcce3fc5016c5215dc9997
Joseph Platta <jwplatta@gmail.com>
2025-01-26 22:59:18 -0600

    remove complex strategy from portfolio class file

--- services/portfolio.rb
+++ services/portfolio.rb
@@ -1,39 +1,23 @@
-class ComplexStrategyPosition
-  attr_reader :strategy, :positions
+require_relative "trades/call_spread"
+require_relative "trades/put_spread"
+require_relative "trades/iron_condor"
 
-  def initialize(strategy:, positions:, exit_threshold: 0.75)
-    # NOTE: right assuming a put spread
-    @strategy = strategy
-    @positions = positions
-    @exit_threshold = exit_threshold
+class Single
+  def initialize(position:)
+    @strategy = "SINGLE"
+    @position = position
   end
 
-  def symbols
-    positions.map(&:symbol)
-  end
+  attr_reader :strategy, :position
 
-  def exitable?
-    progress >= @exit_threshold
-  end
-
-  def progress
-    1 - (exit_price * -1 / entry_price)
-  end
-
-  def entry_price
-    @entry_price ||= (long_leg.average_price * 100 * -1) + (short_leg.average_price * 100)
-  end
-
-  def exit_price
-    @exit_price ||= long_leg.market_value + short_leg.market_value
-  end
-
-  def short_leg
-    @short_leg ||= positions.find { |p| p.short_quantity.positive? }
-  end
-
-  def long_leg
-    @long_leg ||= positions.find { |p| p.long_quantity.positive? }
+  def to_h
+    {
+      strategy: strategy,
+      symbols: position.symbols,
+      entry_price: 0,
+      exit_price: 0,
+      progress: 0
+    }
   end
 end
 
@@ -41,37 +25,105 @@ class Portfolio
   class << self
     def build(orders, account)
       # NOTE: for each order, see if there is an existing position or positions
-      positions = orders.map do |order|
-        curr_position = account.positions.select do |position|
-          order.symbols.include?(position.symbol)
-        end
-
-        if curr_position.count > 1
-          ComplexStrategyPosition.new(
-            strategy: order.complex_order_strategy_type,
-            positions: curr_position
-          )
-        else
-          curr_position.first
-        end
+      # NOTE: match the order leg with the position
+      open_orders = orders.select(&:open?).select do |order|
+        !orders.select(&:close?).any? { |close_order| close_order?(order, close_order) }
       end
 
-      new(
-        positions: positions
-      )
+      open_orders.map do |open_order|
+        if open_order.strategy == "PUT_SPREAD"
+          PutSpread.new(
+            positions: find_positions(open_order, account.positions)
+          )
+        elsif open_order.strategy == "CALL_SPREAD"
+          CallSpread.new(
+            positions: find_positions(open_order, account.positions)
+          )
+        elsif open_order.order_strategy_type == "SINGLE"
+          Single.new(
+            position: find_position(open_order, account.positions)
+          )
+        else
+          raise "Unknown order type: #{open_order.complex_order_strategy_type}"
+        end
+      end.then do |positions|
+        new(
+          positions: positions,
+          cash_balance: account.current_balances.cash_balance,
+          available_funds: account.current_balances.available_funds,
+          buying_power: account.current_balances.buying_power,
+          buying_power_non_marginable_trade: account.current_balances.buying_power_non_marginable_trade,
+          liquidation_value: account.current_balances.liquidation_value,
+          maintenance_requirement: account.current_balances.maintenance_requirement
+        )
+      end
+    end
+
+    def find_positions(order, positions)
+      positions.select { |pos| order.symbols.include? pos.symbol }
+    end
+
+    def find_position(order, positions)
+      positions.find do |pos|
+        order.symbols.count == 1 && order.symbols.include?(pos.symbol)
+      end
+    end
+
+    def close_order?(open_order, close_order)
+      return false if open_order.order_leg_collection.any? { |leg| leg.position_effect == "CLOSING" }
+      return false if close_order.order_leg_collection.any? { |leg| leg.position_effect == "OPENING" }
+      return false unless open_order.complex_order_strategy_type == close_order.complex_order_strategy_type
+      return false unless open_order.filled_quantity == close_order.filled_quantity
+
+      # NOTE:
+      # - symbols must match
+      # - get the first leg of order a, then find the leg in order b with the same symbol
+      # - check that if the first is SELL_TO_OPEN then the other is BUY_TO_CLOSE
+      # or if the first is BUY_TO_OPEN then the other is SELL_TO_CLOSE
+      open_order.order_leg_collection.all? do |open_leg|
+        close_order.order_leg_collection.any? do |close_leg|
+          open_leg.symbol == close_leg.symbol && (
+            (open_leg.instruction == "BUY_TO_OPEN" && close_leg.instruction == "SELL_TO_CLOSE") ||
+            (open_leg.instruction == "SELL_TO_OPEN" && close_leg.instruction == "BUY_TO_CLOSE")
+          )
+        end
+      end
     end
   end
 
-  attr_reader :positions
+  attr_reader :positions, :cash_balance, :available_funds, :buying_power,
+    :buying_power_non_marginable_trade, :liquidation_value, :maintenance_requirement
 
-  def initialize(positions:)
+  def initialize(
+    positions: [], cash_balance: 0, available_funds: 0,
+    buying_power: 0, buying_power_non_marginable_trade: 0,
+    liquidation_value: 0, maintenance_requirement: 0
+  )
     @positions = positions
+    @cash_balance = cash_balance
+    @available_funds = available_funds
+    @buying_power = buying_power
+    @buying_power_non_marginable_trade = buying_power_non_marginable_trade
+    @liquidation_value = liquidation_value
+    @maintenance_requirement = maintenance_requirement
   end
 
   def exitable?
     positions.select(&:exitable?)
   end
 
-  def at_risk?
+  def to_h
+    {
+      positions: positions.map(&:to_h),
+      cash_balance: cash_balance,
+      available_funds: available_funds,
+      buying_power: buying_power,
+      buying_power_non_marginable_trade: buying_power_non_marginable_trade,
+      liquidation_value: liquidation_value,
+      maintenance_requirement: maintenance_requirement
+    }
   end
+
+  # def to_csv
+  # end
 end
