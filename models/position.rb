e3b937619f40343bd60226654dbbf784dd9155b6
Joseph Platta <jwplatta@gmail.com>
2025-02-07 23:20:43 -0600

    remove unused attrs from position class

--- models/position.rb
+++ models/position.rb
@@ -3,10 +3,7 @@ require_relative 'instrument'
 class Position
   attr_reader :short_quantity, :average_price, :current_day_profit_loss, :current_day_profit_loss_percentage,
               :long_quantity, :settled_long_quantity, :settled_short_quantity, :instrument, :market_value,
-              :maintenance_requirement, :average_long_price, :average_short_price, :tax_lot_average_long_price,
-              :tax_lot_average_short_price, :long_open_profit_loss, :short_open_profit_loss,
-              :previous_session_long_quantity, :previous_session_short_quantity, :current_day_cost, :strike,
-              :delta, :mark
+              :maintenance_requirement, :average_long_price, :average_short_price, :long_open_profit_loss, :short_open_profit_loss, :current_day_cost, :strike, :delta, :mark
 
   class << self
     def build(data)
@@ -23,12 +20,8 @@ class Position
         maintenance_requirement: data[:maintenanceRequirement],
         average_long_price: data[:averageLongPrice],
         average_short_price: data[:averageShortPrice],
-        tax_lot_average_long_price: data[:taxLotAverageLongPrice],
-        tax_lot_average_short_price: data[:taxLotAverageShortPrice],
         long_open_profit_loss: data[:longOpenProfitLoss],
         short_open_profit_loss: data[:shortOpenProfitLoss],
-        previous_session_long_quantity: data[:previousSessionLongQuantity],
-        previous_session_short_quantity: data[:previousSessionShortQuantity],
         current_day_cost: data[:currentDayCost]
       )
     end
@@ -36,8 +29,7 @@ class Position
 
   def initialize(
     short_quantity: nil, average_price: nil, current_day_profit_loss: nil, current_day_profit_loss_percentage: nil,
-    long_quantity: nil, settled_long_quantity: nil, settled_short_quantity: nil, instrument: nil, market_value: nil, maintenance_requirement: nil, average_long_price: nil, average_short_price: nil, tax_lot_average_long_price: nil, tax_lot_average_short_price: nil, long_open_profit_loss: nil, short_open_profit_loss: nil, previous_session_long_quantity: nil, previous_session_short_quantity: nil, current_day_cost: nil,
-    strike: nil, delta: nil, mark: nil
+    long_quantity: nil, settled_long_quantity: nil, settled_short_quantity: nil, instrument: nil, market_value: nil, maintenance_requirement: nil, average_long_price: nil, average_short_price: nil, long_open_profit_loss: nil, short_open_profit_loss: nil, current_day_cost: nil, strike: nil, delta: nil, mark: nil
   )
     @short_quantity = short_quantity
     @average_price = average_price
@@ -51,12 +43,8 @@ class Position
     @maintenance_requirement = maintenance_requirement
     @average_long_price = average_long_price
     @average_short_price = average_short_price
-    @tax_lot_average_long_price = tax_lot_average_long_price
-    @tax_lot_average_short_price = tax_lot_average_short_price
     @long_open_profit_loss = long_open_profit_loss
     @short_open_profit_loss = short_open_profit_loss
-    @previous_session_long_quantity = previous_session_long_quantity
-    @previous_session_short_quantity = previous_session_short_quantity
     @current_day_cost = current_day_cost
     @strike = strike
     @delta = delta
