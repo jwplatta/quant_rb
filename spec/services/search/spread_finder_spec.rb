9e25bed261b5bd34d7954d2e6c94e0dddf7bf325
Joseph Platta <jwplatta@gmail.com>
2025-02-25 19:47:46 -0600

    add spec to test that spread finder limits search to a single expiration date

--- spec/services/search/spread_finder_spec.rb
+++ spec/services/search/spread_finder_spec.rb
@@ -19,21 +19,42 @@ RSpec.describe SpreadFinder do
       finder = SpreadFinder.new(
         symbol: "ACME",
         contract_type: "CALL",
-        option_chain: acme_call_options
+        option_chain: acme_call_options,
+        end_date: Date.new(2025, 1, 17)
       )
       best_trade = finder.search
       expect(best_trade.short_leg.strike).to eq 125.0
       expect(best_trade.long_leg.strike).to eq 130.0
+      expect(best_trade.short_leg.expiration_date).to eq best_trade.long_leg.expiration_date
     end
     it "returns a list of put spreads" do
       finder = SpreadFinder.new(
         symbol: "ACME",
         contract_type: "PUT",
-        option_chain: acme_put_options
+        option_chain: acme_put_options,
+        end_date: Date.new(2025, 1, 17)
       )
       best_trade = finder.search
       expect(best_trade.short_leg.strike).to eq 75.0
       expect(best_trade.long_leg.strike).to eq 70.0
+      expect(best_trade.short_leg.expiration_date).to eq best_trade.long_leg.expiration_date
+    end
+    context "when given an expiration date" do
+      it "returns a list of call spreads" do
+        finder = SpreadFinder.new(
+          symbol: "ACME",
+          contract_type: "CALL",
+          option_chain: acme_call_options,
+          min_credit: 50.0,
+          expiration_date: Date.new(2025, 1, 16)
+        )
+        best_trade = finder.search
+
+        expect(best_trade.short_leg.strike).to eq 125.0
+        expect(best_trade.long_leg.strike).to eq 130.0
+        expect(best_trade.short_leg.expiration_date).to eq Date.new(2025, 1, 16)
+        expect(best_trade.short_leg.expiration_date).to eq best_trade.long_leg.expiration_date
+      end
     end
   end
 end
