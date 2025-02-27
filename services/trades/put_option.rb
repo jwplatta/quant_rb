c2997244faa82997e321f93a64b0dd8d2453b2c4
Joseph Platta <jwplatta@gmail.com>
2025-02-26 22:26:42 -0600

    create a put option class

---
+++ services/trades/put_option.rb
@@ -0,0 +1,28 @@
+require_relative 'trade'
+
+class PutOption < Trade
+  attr_reader :symbol, :strike, :delta, :mark, :ask, :bid, :expiration_date, :quantity
+
+  def initialize(symbol, strike, delta, mark, ask, bid, expiration_date, quantity)
+    @symbol = symbol
+    @strike = strike
+    @delta = delta
+    @mark = mark
+    @ask = ask
+    @bid = bid
+    @expiration_date = expiration_date
+    @quantity = quantity
+  end
+
+  def quantity=(new_quantity)
+    @quantity = new_quantity
+  end
+
+  def instruction
+    if quantity < 0
+      "SELL_TO_OPEN"
+    elsif quantity > 0
+      "BUY_TO_CLOSE"
+    end
+  end
+end
