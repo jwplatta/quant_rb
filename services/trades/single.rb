78d246c17ceace979f2fa5eb6d25040a90e621f6
Joseph Platta <jwplatta@gmail.com>
2025-02-27 23:49:27 -0600

    remove unused class

--- services/trades/single.rb
+++
@@ -1,7 +0,0 @@
-class Single < Trade
-  def initialize(position:, exit_threshold: 0.75, approx_fees: 0.0)
-    @strategy = "SINGLE"
-    @position = position
-    @exit_threshold = exit_threshold
-  end
-end
