bba34dd257369fdbf324d3f86b6a3af4984a9a67
Joseph Platta <jwplatta@gmail.com>
2025-03-01 00:07:16 -0600

    move schwab module classes to mixins folder

---
+++ mixins/schwab/orders/iron_condor_order.rb
@@ -0,0 +1,42 @@
+require "schwab_rb"
+
+class IronCondorOrder
+  class << self
+    def build(trade, **options)
+      schwab_order_builder.new.tap do |builder|
+        builder.set_account_number(options[:account_number])
+        builder.set_order_strategy_type("SINGLE")
+        builder.set_session(SchwabRb::Orders::Session::NORMAL)
+        builder.set_duration(SchwabRb::Orders::Duration::DAY)
+        builder.set_order_type(SchwabRb::Order::Types::NET_CREDIT)
+        builder.set_complex_order_strategy_type(SchwabRb::Order::ComplexOrderStrategyTypes::IRON_CONDOR)
+        builder.set_quantity(options[:quantity])
+        builder.set_price(trade.credit_debit)
+        builder.add_option_leg(
+          SchwabRb::Orders::OptionInstructions::SELL_TO_OPEN,
+          trade.put_spread.short_leg.symbol,
+          options[:quantity]
+        )
+        builder.add_option_leg(
+          SchwabRb::Orders::OptionInstructions::BUY_TO_OPEN,
+          trade.put_spread.long_leg.symbol,
+          options[:quantity]
+        )
+        builder.add_option_leg(
+          SchwabRb::Orders::OptionInstructions::SELL_TO_OPEN,
+          trade.call_spread.short_leg.symbol,
+          options[:quantity]
+        )
+        builder.add_option_leg(
+          SchwabRb::Orders::OptionInstructions::BUY_TO_OPEN,
+          trade.call_spread.long_leg.symbol,
+          options[:quantity]
+        )
+      end
+    end
+
+    def schwab_order_builder
+      SchwabRb::Orders::Builder
+    end
+  end
+end
