e160f5f6625307658dbafe3d0858dd8a36edf171
Joseph Platta <jwplatta@gmail.com>
2025-03-01 09:57:43 -0600

    replace cache attr with refresh method

--- services/vix_checker.rb
+++ services/vix_checker.rb
@@ -5,8 +5,6 @@ require_relative "../mixins/schwab/schwab"
 class VIXChecker
   include Schwab
 
-  attr_reader :cache
-
   module VIXThresholds
     LOW=12.0
     HIGH=20.0
@@ -20,10 +18,6 @@ class VIXChecker
 
   VIX_SYMBOL = "$VIX"
 
-  def initialize(cache: false)
-    @cache = cache
-  end
-
   def check
     if vix_quote.mark <= VIXThresholds::LOW
       VIXStatusNames::LOW
@@ -34,16 +28,11 @@ class VIXChecker
     end
   end
 
-  def reset
+  def refresh
     @vix_quote = nil
   end
 
   def vix_quote
-    # REVIEW: the schwab mixin should return the data objects
-    if cache
-      @vix_quote ||= quote(VIX_SYMBOL)
-    else
-      quote(VIX_SYMBOL)
-    end
+    @vix_quote ||= quote(VIX_SYMBOL)
   end
 end
