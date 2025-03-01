89259947fedf8cb7f250245aacc5b2759582ae97
Joseph Platta <jwplatta@gmail.com>
2025-03-01 12:28:56 -0600

    remove schwab mixin because it is in the base class

--- services/trades/put_option.rb
+++ services/trades/put_option.rb
@@ -1,9 +1,6 @@
 require_relative "trade"
-require_relative "../../mixins/schwab/schwab"
 
 class PutOption < Trade
-  include Schwab
-
   class << self
     def from_h(hash)
       expiration_date = hash[:expiration_date] ? Date.parse(hash[:expiration_date]) : nil
