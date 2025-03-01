6c4cde2a995ad7b31242389eae9117677ac3d8a6
Joseph Platta <jwplatta@gmail.com>
2025-03-01 12:27:29 -0600

    include the schwab mixin in the trade base class and add instance vars for tracking order data

--- services/trades/trade.rb
+++ services/trades/trade.rb
@@ -1,27 +1,83 @@
-require 'schwab_rb'
+require_relative "../../mixins/schwab/schwab"
+
+Order = Struct.new(:id, :status, :date)
 
 class Trade
-  attr_accessor :increment, :round, :open_credit_debit, :open_date, :open_fees, :open_commission
+  include Schwab
 
-  def initialize(increment: 0.01, round: 2, open_credit_debit: nil, open_date: nil, open_fees: 0.0, open_commission: 0.0)
+  attr_accessor :increment, :round, :open_credit_debit, :open_date,
+    :open_fees, :open_commission, :exit_threshold, :max_loss, :quantity,
+    :order_id, :order_status, :transactions, :underlying_symbol
+
+  module Actions
+    EXIT_LOSS="EXIT_LOSS"
+    EXIT_PROFIT="EXIT_PROFIT"
+    HOLD="HOLD"
+    ADJUST="ADJUST"
+    ADJUST_PUT="ADJUST_PUT"
+    ADJUST_CALL="ADJUST_CALL"
+    ROLL_UP="ROLL_UP"
+    ROLL_DOWN="ROLL_AWAY"
+    ROLL_OUT="ROLL_OUT"
+  end
+
+  def initialize(
+    underlying_symbol: nil, increment: 0.01,
+    round: 2, open_credit_debit: nil, open_date: nil,
+    open_fees: 0.0, open_commission: 0.0,
+    exit_threshold: 0.75, max_loss: -3.0
+  )
+    @underlying_symbol = underlying_symbol
     @increment = increment
     @round = round
     @open_credit_debit = open_credit_debit
     @open_date = open_date
     @open_fees = open_fees
     @open_commission = open_commission
-  end
-
-  def complex?
-    false
+    @exit_threshold = exit_threshold
+    @max_loss = max_loss
+    @quantity = 1
+    @order_id = nil
+    @order_status = nil
+    @transactions = []
   end
 
   def credit_debit
     raise "Must be implemented in subclass"
   end
 
+  def net_credit_debit
+    raise "Must be implemented in subclass"
+  end
+
   def delta
     raise "Must be implemented in subclass"
+  end
+
+  def check_market
+    raise "Must be implemented in subclass"
+  end
+
+  def order_id=(id)
+    @order_id = id
+  end
+
+  def add_transaction(transaction)
+    @transactions << transaction
+  end
+
+  def exitable?
+    progress >= exit_threshold || progress <= max_loss
+  end
+
+  def progress
+    return nil unless open_credit_debit
+
+    (net_credit_debit - net_open_credit_debit) / net_open_credit_debit
+  end
+
+  def net_open_credit_debit
+    open_credit_debit * 100 - open_fees - open_commission
   end
 
   def open_credit_debit=(credit_debit)
