d7245d085e4ad994073c7184e64b8573a2bc7bbb
Joseph Platta <jwplatta@gmail.com>
2025-02-28 23:54:09 -0600

    remove quote from attr names

--- services/schwab/data_objects/quote.rb
+++ services/schwab/data_objects/quote.rb
@@ -1,8 +1,6 @@
 module DataObjects
   class QuoteFactory
-    def self.build(data)
-      symbol, quote_data = data.first
-
+    def self.build(quote_data)
       case quote_data[:assetMainType]
       when "OPTION"
         DataObjects::OptionQuote.new(quote_data)
@@ -17,7 +15,7 @@ module DataObjects
   end
 
   class OptionQuote
-    attr_reader :symbol, :asset_main_type, :realtime, :ssid, :quote_52_week_high, :quote_52_week_low, :quote_ask_price, :quote_ask_size, :quote_bid_price, :quote_bid_size, :quote_close_price, :quote_delta, :quote_gamma, :quote_high_price, :quote_ind_ask_price, :quote_ind_bid_price, :quote_ind_quote_time, :quote_implied_yield, :quote_last_price, :quote_last_size, :quote_low_price, :quote_mark, :quote_mark_change, :quote_mark_percent_change, :quote_money_intrinsic_value, :quote_net_change, :quote_net_percent_change, :quote_open_interest, :quote_open_price, :quote_quote_time, :quote_rho, :quote_security_status, :quote_theoretical_option_value, :quote_theta, :quote_time_value, :quote_total_volume, :quote_trade_time, :quote_underlying_price, :quote_vega, :quote_volatility, :contract_type, :days_to_expiration, :deliverables, :description, :exchange, :exchange_name, :exercise_type, :expiration_day, :expiration_month, :expiration_type, :expiration_year, :is_penny_pilot, :last_trading_day, :multiplier, :settlement_type, :strike_price, :underlying, :underlying_asset_type
+    attr_reader :symbol, :asset_main_type, :realtime, :ssid, :quote_52_week_high, :quote_52_week_low, :ask_price, :ask_size, :bid_price, :bid_size, :close_price, :delta, :gamma, :high_price, :ind_ask_price, :ind_bid_price, :ind_time, :implied_yield, :last_price, :last_size, :low_price, :mark, :mark_change, :mark_percent_change, :money_intrinsic_value, :net_change, :net_percent_change, :open_interest, :open_price, :time, :rho, :security_status, :theoretical_option_value, :theta, :time_value, :total_volume, :trade_time, :underlying_price, :vega, :volatility, :contract_type, :days_to_expiration, :deliverables, :description, :exchange, :exchange_name, :exercise_type, :expiration_day, :expiration_month, :expiration_type, :expiration_year, :is_penny_pilot, :last_trading_day, :multiplier, :settlement_type, :strike_price, :underlying, :underlying_asset_type
 
     def initialize(data)
       @symbol = data[:symbol]
@@ -26,40 +24,40 @@ module DataObjects
       @ssid = data[:ssid]
       @quote_52_week_high = data.dig(:quote, :"52WeekHigh")
       @quote_52_week_low = data.dig(:quote, :"52WeekLow")
-      @quote_ask_price = data.dig(:quote, :askPrice)
-      @quote_ask_size = data.dig(:quote, :askSize)
-      @quote_bid_price = data.dig(:quote, :bidPrice)
-      @quote_bid_size = data.dig(:quote, :bidSize)
-      @quote_close_price = data.dig(:quote, :closePrice)
-      @quote_delta = data.dig(:quote, :delta)
-      @quote_gamma = data.dig(:quote, :gamma)
-      @quote_high_price = data.dig(:quote, :highPrice)
-      @quote_ind_ask_price = data.dig(:quote, :indAskPrice)
-      @quote_ind_bid_price = data.dig(:quote, :indBidPrice)
-      @quote_ind_quote_time = data.dig(:quote, :indQuoteTime)
-      @quote_implied_yield = data.dig(:quote, :impliedYield)
-      @quote_last_price = data.dig(:quote, :lastPrice)
-      @quote_last_size = data.dig(:quote, :lastSize)
-      @quote_low_price = data.dig(:quote, :lowPrice)
-      @quote_mark = data.dig(:quote, :mark)
-      @quote_mark_change = data.dig(:quote, :markChange)
-      @quote_mark_percent_change = data.dig(:quote, :markPercentChange)
-      @quote_money_intrinsic_value = data.dig(:quote, :moneyIntrinsicValue)
-      @quote_net_change = data.dig(:quote, :netChange)
-      @quote_net_percent_change = data.dig(:quote, :netPercentChange)
-      @quote_open_interest = data.dig(:quote, :openInterest)
-      @quote_open_price = data.dig(:quote, :openPrice)
-      @quote_quote_time = data.dig(:quote, :quoteTime)
-      @quote_rho = data.dig(:quote, :rho)
-      @quote_security_status = data.dig(:quote, :securityStatus)
-      @quote_theoretical_option_value = data.dig(:quote, :theoreticalOptionValue)
-      @quote_theta = data.dig(:quote, :theta)
-      @quote_time_value = data.dig(:quote, :timeValue)
-      @quote_total_volume = data.dig(:quote, :totalVolume)
-      @quote_trade_time = data.dig(:quote, :tradeTime)
-      @quote_underlying_price = data.dig(:quote, :underlyingPrice)
-      @quote_vega = data.dig(:quote, :vega)
-      @quote_volatility = data.dig(:quote, :volatility)
+      @ask_price = data.dig(:quote, :askPrice)
+      @ask_size = data.dig(:quote, :askSize)
+      @bid_price = data.dig(:quote, :bidPrice)
+      @bid_size = data.dig(:quote, :bidSize)
+      @close_price = data.dig(:quote, :closePrice)
+      @delta = data.dig(:quote, :delta)
+      @gamma = data.dig(:quote, :gamma)
+      @high_price = data.dig(:quote, :highPrice)
+      @ind_ask_price = data.dig(:quote, :indAskPrice)
+      @ind_bid_price = data.dig(:quote, :indBidPrice)
+      @ind_quote_time = data.dig(:quote, :indQuoteTime)
+      @implied_yield = data.dig(:quote, :impliedYield)
+      @last_price = data.dig(:quote, :lastPrice)
+      @last_size = data.dig(:quote, :lastSize)
+      @low_price = data.dig(:quote, :lowPrice)
+      @mark = data.dig(:quote, :mark)
+      @mark_change = data.dig(:quote, :markChange)
+      @mark_percent_change = data.dig(:quote, :markPercentChange)
+      @money_intrinsic_value = data.dig(:quote, :moneyIntrinsicValue)
+      @net_change = data.dig(:quote, :netChange)
+      @net_percent_change = data.dig(:quote, :netPercentChange)
+      @open_interest = data.dig(:quote, :openInterest)
+      @open_price = data.dig(:quote, :openPrice)
+      @quote_time = data.dig(:quote, :quoteTime)
+      @rho = data.dig(:quote, :rho)
+      @security_status = data.dig(:quote, :securityStatus)
+      @theoretical_option_value = data.dig(:quote, :theoreticalOptionValue)
+      @theta = data.dig(:quote, :theta)
+      @time_value = data.dig(:quote, :timeValue)
+      @total_volume = data.dig(:quote, :totalVolume)
+      @trade_time = data.dig(:quote, :tradeTime)
+      @underlying_price = data.dig(:quote, :underlyingPrice)
+      @vega = data.dig(:quote, :vega)
+      @volatility = data.dig(:quote, :volatility)
       @contract_type = data.dig(:reference, :contractType)
       @days_to_expiration = data.dig(:reference, :daysToExpiration)
       @deliverables = data.dig(:reference, :deliverables)
@@ -92,7 +90,7 @@ module DataObjects
   end
 
   class IndexQuote
-    attr_reader :symbol, :asset_main_type, :realtime, :ssid, :avg_10_days_volume, :avg_1_year_volume, :div_amount, :div_freq, :div_pay_amount, :div_yield, :eps, :fund_leverage_factor, :pe_ratio, :quote_52_week_high, :quote_52_week_low, :quote_close_price, :quote_high_price, :quote_last_price, :quote_low_price, :quote_net_change, :quote_net_percent_change, :quote_open_price, :quote_security_status, :quote_total_volume, :quote_trade_time, :description, :exchange, :exchange_name
+    attr_reader :symbol, :asset_main_type, :realtime, :ssid, :avg_10_days_volume, :avg_1_year_volume, :div_amount, :div_freq, :div_pay_amount, :div_yield, :eps, :fund_leverage_factor, :pe_ratio, :quote_52_week_high, :quote_52_week_low, :close_price, :high_price, :last_price, :low_price, :net_change, :net_percent_change, :open_price, :security_status, :total_volume, :trade_time, :description, :exchange, :exchange_name
 
     def initialize(data)
       @symbol = data[:symbol]
@@ -110,28 +108,28 @@ module DataObjects
       @pe_ratio = data.dig(:fundamental, :peRatio)
       @quote_52_week_high = data.dig(:quote, :"52WeekHigh")
       @quote_52_week_low = data.dig(:quote, :"52WeekLow")
-      @quote_close_price = data.dig(:quote, :closePrice)
-      @quote_high_price = data.dig(:quote, :highPrice)
-      @quote_last_price = data.dig(:quote, :lastPrice)
-      @quote_low_price = data.dig(:quote, :lowPrice)
-      @quote_net_change = data.dig(:quote, :netChange)
-      @quote_net_percent_change = data.dig(:quote, :netPercentChange)
-      @quote_open_price = data.dig(:quote, :openPrice)
-      @quote_security_status = data.dig(:quote, :securityStatus)
-      @quote_total_volume = data.dig(:quote, :totalVolume)
-      @quote_trade_time = data.dig(:quote, :tradeTime)
+      @close_price = data.dig(:quote, :closePrice)
+      @high_price = data.dig(:quote, :highPrice)
+      @last_price = data.dig(:quote, :lastPrice)
+      @low_price = data.dig(:quote, :lowPrice)
+      @net_change = data.dig(:quote, :netChange)
+      @net_percent_change = data.dig(:quote, :netPercentChange)
+      @open_price = data.dig(:quote, :openPrice)
+      @security_status = data.dig(:quote, :securityStatus)
+      @total_volume = data.dig(:quote, :totalVolume)
+      @trade_time = data.dig(:quote, :tradeTime)
       @description = data.dig(:reference, :description)
       @exchange = data.dig(:reference, :exchange)
       @exchange_name = data.dig(:reference, :exchangeName)
     end
 
     def mark
-      (@quote_high_price + @quote_low_price) / 2
+      (@high_price + @low_price) / 2.0
     end
   end
 
   class EquityQuote
-    attr_reader :symbol, :asset_main_type, :asset_sub_type, :quote_type, :realtime, :ssid, :extended_ask_price, :extended_ask_size, :extended_bid_price, :extended_bid_size, :extended_last_price, :extended_last_size, :extended_mark, :extended_quote_time, :extended_total_volume, :extended_trade_time, :avg_10_days_volume, :avg_1_year_volume, :declaration_date, :div_amount, :div_ex_date, :div_freq, :div_pay_amount, :div_pay_date, :div_yield, :eps, :fund_leverage_factor, :last_earnings_date, :next_div_ex_date, :next_div_pay_date, :pe_ratio, :quote_52_week_high, :quote_52_week_low, :quote_ask_mic_id, :quote_ask_price, :quote_ask_size, :quote_ask_time, :quote_bid_mic_id, :quote_bid_price, :quote_bid_size, :quote_bid_time, :quote_close_price, :quote_high_price, :quote_last_mic_id, :quote_last_price, :quote_last_size, :quote_low_price, :quote_mark, :quote_mark_change, :quote_mark_percent_change, :quote_net_change, :quote_net_percent_change, :quote_open_price, :quote_post_market_change, :quote_post_market_percent_change, :quote_quote_time, :quote_security_status, :quote_total_volume, :quote_trade_time, :cusip, :description, :exchange, :exchange_name, :is_hard_to_borrow, :is_shortable, :htb_rate, :market_last_price, :market_last_size, :market_net_change, :market_percent_change, :market_trade_time
+    attr_reader :symbol, :asset_main_type, :asset_sub_type, :quote_type, :realtime, :ssid, :extended_ask_price, :extended_ask_size, :extended_bid_price, :extended_bid_size, :extended_last_price, :extended_last_size, :extended_mark, :extended_quote_time, :extended_total_volume, :extended_trade_time, :avg_10_days_volume, :avg_1_year_volume, :declaration_date, :div_amount, :div_ex_date, :div_freq, :div_pay_amount, :div_pay_date, :div_yield, :eps, :fund_leverage_factor, :last_earnings_date, :next_div_ex_date, :next_div_pay_date, :pe_ratio, :quote_52_week_high, :quote_52_week_low, :ask_mic_id, :ask_price, :ask_size, :ask_time, :bid_mic_id, :bid_price, :bid_size, :bid_time, :close_price, :high_price, :last_mic_id, :last_price, :last_size, :low_price, :mark, :mark_change, :mark_percent_change, :net_change, :net_percent_change, :open_price, :post_market_change, :post_market_percent_change, :time, :security_status, :total_volume, :trade_time, :cusip, :description, :exchange, :exchange_name, :is_hard_to_borrow, :is_shortable, :htb_rate, :market_last_price, :market_last_size, :market_net_change, :market_percent_change, :market_trade_time
 
     def initialize(data)
       @symbol = data[:symbol]
@@ -167,32 +165,32 @@ module DataObjects
       @pe_ratio = data.dig(:fundamental, :peRatio)
       @quote_52_week_high = data.dig(:quote, :"52WeekHigh")
       @quote_52_week_low = data.dig(:quote, :"52WeekLow")
-      @quote_ask_mic_id = data.dig(:quote, :askMICId)
-      @quote_ask_price = data.dig(:quote, :askPrice)
-      @quote_ask_size = data.dig(:quote, :askSize)
-      @quote_ask_time = data.dig(:quote, :askTime)
-      @quote_bid_mic_id = data.dig(:quote, :bidMICId)
-      @quote_bid_price = data.dig(:quote, :bidPrice)
-      @quote_bid_size = data.dig(:quote, :bidSize)
-      @quote_bid_time = data.dig(:quote, :bidTime)
-      @quote_close_price = data.dig(:quote, :closePrice)
-      @quote_high_price = data.dig(:quote, :highPrice)
-      @quote_last_mic_id = data.dig(:quote, :lastMICId)
-      @quote_last_price = data.dig(:quote, :lastPrice)
-      @quote_last_size = data.dig(:quote, :lastSize)
-      @quote_low_price = data.dig(:quote, :lowPrice)
-      @quote_mark = data.dig(:quote, :mark)
-      @quote_mark_change = data.dig(:quote, :markChange)
-      @quote_mark_percent_change = data.dig(:quote, :markPercentChange)
-      @quote_net_change = data.dig(:quote, :netChange)
-      @quote_net_percent_change = data.dig(:quote, :netPercentChange)
-      @quote_open_price = data.dig(:quote, :openPrice)
-      @quote_post_market_change = data.dig(:quote, :postMarketChange)
-      @quote_post_market_percent_change = data.dig(:quote, :postMarketPercentChange)
-      @quote_quote_time = data.dig(:quote, :quoteTime)
-      @quote_security_status = data.dig(:quote, :securityStatus)
-      @quote_total_volume = data.dig(:quote, :totalVolume)
-      @quote_trade_time = data.dig(:quote, :tradeTime)
+      @ask_mic_id = data.dig(:quote, :askMICId)
+      @ask_price = data.dig(:quote, :askPrice)
+      @ask_size = data.dig(:quote, :askSize)
+      @ask_time = data.dig(:quote, :askTime)
+      @bid_mic_id = data.dig(:quote, :bidMICId)
+      @bid_price = data.dig(:quote, :bidPrice)
+      @bid_size = data.dig(:quote, :bidSize)
+      @bid_time = data.dig(:quote, :bidTime)
+      @close_price = data.dig(:quote, :closePrice)
+      @high_price = data.dig(:quote, :highPrice)
+      @last_mic_id = data.dig(:quote, :lastMICId)
+      @last_price = data.dig(:quote, :lastPrice)
+      @last_size = data.dig(:quote, :lastSize)
+      @low_price = data.dig(:quote, :lowPrice)
+      @mark = data.dig(:quote, :mark)
+      @mark_change = data.dig(:quote, :markChange)
+      @mark_percent_change = data.dig(:quote, :markPercentChange)
+      @net_change = data.dig(:quote, :netChange)
+      @net_percent_change = data.dig(:quote, :netPercentChange)
+      @open_price = data.dig(:quote, :openPrice)
+      @post_market_change = data.dig(:quote, :postMarketChange)
+      @post_market_percent_change = data.dig(:quote, :postMarketPercentChange)
+      @quote_time = data.dig(:quote, :quoteTime)
+      @security_status = data.dig(:quote, :securityStatus)
+      @total_volume = data.dig(:quote, :totalVolume)
+      @trade_time = data.dig(:quote, :tradeTime)
       @cusip = data.dig(:reference, :cusip)
       @description = data.dig(:reference, :description)
       @exchange = data.dig(:reference, :exchange)
