8903de06e71e21287aa6fb382ac9aae9b02d47cf
Joseph Platta <jwplatta@gmail.com>
2025-03-02 00:09:32 -0600

    add a leg instructions for position method to handle entry and exit order instructions

--- mixins/schwab/orders/iron_condor_order.rb
+++ mixins/schwab/orders/iron_condor_order.rb
@@ -3,6 +3,8 @@ require "schwab_rb"
 class IronCondorOrder
   class << self
     def build(trade, **options)
+      position_type = options[:position_type] || :entry
+
       schwab_order_builder.new.tap do |builder|
         builder.set_account_number(options[:account_number])
         builder.set_order_strategy_type("SINGLE")
@@ -12,26 +14,48 @@ class IronCondorOrder
         builder.set_complex_order_strategy_type(SchwabRb::Order::ComplexOrderStrategyTypes::IRON_CONDOR)
         builder.set_quantity(options[:quantity])
         builder.set_price(trade.credit_debit)
+
+        instructions = leg_instructions_for_position(position_type)
+
         builder.add_option_leg(
-          SchwabRb::Orders::OptionInstructions::SELL_TO_OPEN,
+          instructions[:put_short],
           trade.put_spread.short_leg.symbol,
           options[:quantity]
         )
         builder.add_option_leg(
-          SchwabRb::Orders::OptionInstructions::BUY_TO_OPEN,
+          instructions[:put_long],
           trade.put_spread.long_leg.symbol,
           options[:quantity]
         )
         builder.add_option_leg(
-          SchwabRb::Orders::OptionInstructions::SELL_TO_OPEN,
+          instructions[:call_short],
           trade.call_spread.short_leg.symbol,
           options[:quantity]
         )
         builder.add_option_leg(
-          SchwabRb::Orders::OptionInstructions::BUY_TO_OPEN,
+          instructions[:call_long],
           trade.call_spread.long_leg.symbol,
           options[:quantity]
         )
+      end
+    end
+
+    def leg_instructions_for_position(position_type)
+      case position_type
+      when :entry
+        {
+          put_short: SchwabRb::Orders::OptionInstructions::SELL_TO_OPEN,
+          put_long: SchwabRb::Orders::OptionInstructions::BUY_TO_OPEN,
+          call_short: SchwabRb::Orders::OptionInstructions::SELL_TO_OPEN,
+          call_long: SchwabRb::Orders::OptionInstructions::BUY_TO_OPEN
+        }
+      when :exit
+        {
+          put_short: SchwabRb::Orders::OptionInstructions::BUY_TO_CLOSE,
+          put_long: SchwabRb::Orders::OptionInstructions::SELL_TO_CLOSE,
+          call_short: SchwabRb::Orders::OptionInstructions::BUY_TO_CLOSE,
+          call_long: SchwabRb::Orders::OptionInstructions::SELL_TO_CLOSE
+        }
       end
     end
 
