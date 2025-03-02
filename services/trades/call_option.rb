4a41473f57654574849b239b684c5fef65c9658a
Joseph Platta <jwplatta@gmail.com>
2025-03-02 00:10:14 -0600

    make strike a named param and pass quantity to super

--- services/trades/call_option.rb
+++ services/trades/call_option.rb
@@ -6,7 +6,7 @@ class CallOption < Trade
       expiration_date = hash[:expiration_date] ? Date.parse(hash[:expiration_date]) : nil
       CallOption.new(
         hash[:symbol],
-        hash[:strike],
+        strike: hash[:strike],
         expiration_date: expiration_date,
         quantity: hash.fetch(:quantity, nil),
         open_credit_debit: hash.fetch(:open_credit_debit, nil),
@@ -24,7 +24,7 @@ class CallOption < Trade
       )
       CallOption.new(
         quote.symbol,
-        quote.strike_price,
+        strike: quote.strike_price,
         delta: quote.delta,
         mark: quote.mark,
         ask: quote.ask_price,
@@ -34,10 +34,10 @@ class CallOption < Trade
     end
   end
 
-  attr_reader :symbol, :strike, :delta, :mark, :ask, :bid, :expiration_date, :quantity
+  attr_reader :symbol, :strike, :delta, :mark, :ask, :bid, :expiration_date
 
   def initialize(
-    symbol, strike, delta: 999, mark: nil, ask: nil, bid: nil, expiration_date: nil, quantity: nil,
+    symbol, strike: nil, delta: 999, mark: nil, ask: nil, bid: nil, expiration_date: nil, quantity: nil,
     open_credit_debit: nil, open_date: nil, open_fees: nil, open_commission: nil,
     increment: 0.01, round: 2
   )
@@ -47,7 +47,8 @@ class CallOption < Trade
       open_credit_debit: open_credit_debit,
       open_date: open_date,
       open_fees: open_fees,
-      open_commission: open_commission
+      open_commission: open_commission,
+      quantity: quantity
     )
     @strategy = "SINGLE"
     @symbol = symbol
@@ -57,7 +58,6 @@ class CallOption < Trade
     @ask = ask
     @bid = bid
     @expiration_date = expiration_date
-    @quantity = quantity
   end
 
   def risk_status
@@ -116,15 +116,21 @@ class CallOption < Trade
       @mark = quote.mark
       @ask = quote.ask_price
       @bid = quote.bid_price
+      @strike = quote.strike_price
+      @expiration_date = Date.new(
+        quote.expiration_year,
+        quote.expiration_month,
+        quote.expiration_day
+      )
     end
   end
 
   def to_h
     {
       type: "CALL",
-      strategy: "SIGNLE",
       symbol: symbol,
       strike: strike,
+      quantity: quantity,
       expiration_date: expiration_date,
       open_credit_debit: open_credit_debit,
       open_date: open_date,
