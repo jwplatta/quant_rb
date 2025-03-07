2953d8987dd5ce11db9570e80fd1a57280937642
Joseph Platta <jwplatta@gmail.com>
2025-03-06 23:49:17 -0600

    WIP: adding setter methods to orderable mixin

--- mixins/orderable.rb
+++ mixins/orderable.rb
@@ -4,16 +4,46 @@ module Orderable
   include Schwab
 
   attr_reader :order_id, :order_status, :open_date,
-    :open_credit_debit, :open_fees, :open_commission
+    :open_credit_debit, :open_fees, :open_commission,
+    :order_rejects, :transactions
+
+  def order_id=(id)
+    @order_id = id
+  end
+
+  def open_credit_debit=(credit_debit)
+    @open_credit_debit = credit_debit
+  end
+
+  def open_date=(date)
+    @open_date = date
+  end
+
+  def open_fees=(fees)
+    @open_fees = fees
+  end
+
+  def open_commission=(commission)
+    @open_commission = commission
+  end
+
+  def add_transaction(transaction)
+    @transactions ||= [] if @transactions.nil?
+    @transactions << transaction
+  end
 
   def preview(position_type: :entry)
-    build_and_preview_order(self, quantity: quantity, position_type: position_type).then do |order|
-      if order.accepted?
+    build_and_preview_order(self, quantity: quantity, position_type: position_type).then do |order_preview|
+      if order_preview.accepted?
         @open_date = Date.today
         @open_credit_debit = credit_debit
-        @open_fees = order.fees
-        @open_commission = order.commission
-        @order_status = order.order_strategy.status
+        @open_fees = order_preview.fees
+        @open_commission = order_preview.commission
+        @order_status = order_preview.order_strategy.status
+        @order_rejects = []
+      else
+        @order_status = order_preview.order_strategy.status
+        @order_rejects = order_preview.order_validation_result.rejects(&:activity_message)
       end
     end
   end
@@ -48,13 +78,13 @@ module Orderable
         @open_commission = nil
       elsif ["REJECTED", "EXPIRED", "CANCELED"].include? order.status
         @order_id = nil
-        @order_status = nil
+        @order_status = order.status
         @open_date = nil
         @open_credit_debit = nil
         @open_fees = nil
         @open_commission = nil
       else ["PENDING_ACTIVATION", "WORKING"].include? order.status
-        puts "Order still pending"
+        @order_status = order.status
       end
     end
   end
