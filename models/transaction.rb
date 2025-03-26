85d64fad3b320520168cbfc13621dbfb79bb79b0
Joseph Platta <jwplatta@gmail.com>
2025-03-26 00:30:54 -0500

    add a transaction model

--- models/transaction.rb
+++ models/transaction.rb
@@ -1,17 +1,21 @@
 require "active_record"
 
-class Transaction < ActiveRecord::Base
-  belongs_to :order
+module DB
+  class Transaction < ActiveRecord::Base
+    self.table_name = 'transactions'
+    
+    belongs_to :order, class_name: 'DB::Order'
 
-  validates :symbol, presence: true
-  validates :description, presence: true
-  validates :put_call, presence: true
-  validates :trade_date, presence: true
-  validates :instrument_id, presence: true
-  validates :quantity, presence: true
-  validates :fees, presence: true
-  validates :commission, presence: true
-  validates :cost, presence: true
-  validates :net_amount, presence: true
-  validates :position_effect, presence: true
+    validates :symbol, presence: true
+    validates :description, presence: true
+    validates :put_call, presence: true
+    validates :trade_date, presence: true
+    validates :instrument_id, presence: true
+    validates :quantity, presence: true
+    validates :fees, presence: true
+    validates :commission, presence: true
+    validates :cost, presence: true
+    validates :net_amount, presence: true
+    validates :position_effect, presence: true
+  end
 end
