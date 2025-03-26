bd0504403302e54e268bb66d9cdf15c978fbd76d
Joseph Platta <jwplatta@gmail.com>
2025-03-26 00:33:57 -0500

    add order model

--- models/order.rb
+++ models/order.rb
@@ -1,13 +1,16 @@
 require "active_record"
 
-class Order < ActiveRecord::Base
-  belongs_to :trade
-  has_many :transactions
+module DB
+  class Order < ActiveRecord::Base
+    self.table_name = 'orders'
+    
+    belongs_to :trade, class_name: 'DB::Trade'
+    has_many :transactions, class_name: 'DB::Transaction', foreign_key: 'order_id'
 
-  validates :order_id, presence: true
-  validates :order_type, presence: true
-  validates :underlying, presence: true
-  validates :status, presence: true
-  validates :strategy_type, presence: true
-  validates :underlying, presence: true
+    validates :order_id, presence: true
+    validates :order_type, presence: true
+    validates :underlying, presence: true
+    validates :status, presence: true
+    validates :strategy_type, presence: true
+  end
 end
