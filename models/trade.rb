dffe9be11b847b5dbb2c3dff7f6aa405c64cbd20
Joseph Platta <jwplatta@gmail.com>
2025-03-26 00:31:12 -0500

    add a trade model

--- models/trade.rb
+++ models/trade.rb
@@ -1,11 +1,15 @@
 require "active_record"
 
-class Trade < ActiveRecord::Base
-  has_many :legs
-  has_many :orders
-  has_many :transactions, through: :orders
+module DB
+  class Trade < ActiveRecord::Base
+    self.table_name = 'trades'
+    
+    has_many :legs, class_name: 'DB::TradeLeg', foreign_key: 'trade_id'
+    has_many :orders, class_name: 'DB::Order', foreign_key: 'trade_id'
+    has_many :transactions, through: :orders, class_name: 'DB::Transaction'
 
-  validates :underlying, presence: true
-  validates :strategy_type, presence: true
-  validates :open_date, presence: true
+    validates :underlying, presence: true
+    validates :strategy, presence: true
+    validates :open_date, presence: true
+  end
 end
