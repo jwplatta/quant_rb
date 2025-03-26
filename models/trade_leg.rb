04a54582e376e7a0d7ccc51c5709ca8de4e98932
Joseph Platta <jwplatta@gmail.com>
2025-03-26 00:31:21 -0500

    add a trade leg model

---
+++ models/trade_leg.rb
@@ -0,0 +1,18 @@
+require "active_record"
+
+module DB
+  class TradeLeg < ActiveRecord::Base
+    self.table_name = 'trade_legs'
+    
+    belongs_to :trade, class_name: 'DB::Trade'
+
+    validates :put_call, presence: true
+    validates :symbol, presence: true
+    validates :mark, presence: true
+    validates :ask, presence: true
+    validates :bid, presence: true
+    validates :delta, presence: true
+    validates :expiration_date, presence: true
+    validates :instruction, presence: true
+  end
+end
