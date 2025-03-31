6a0f33c3dbc00f9bba513f2295421f14e49c0a6e
Joseph Platta <jwplatta@gmail.com>
2025-03-30 23:23:56 -0500

    refactor base trade with orderable mixin changes

--- services/trades/trade.rb
+++ services/trades/trade.rb
@@ -7,24 +7,9 @@ module Services
   module Trades
     class Trade
       include Orderable
-      # include Logger
-      # include Adjustable
 
-      attr_accessor :increment, :round, :open_credit_debit, :open_date,
-        :open_fees, :open_commission, :exit_threshold, :max_loss, :quantity,
-        :order_id, :order_status, :order_rejects, :transactions, :underlying_symbol
-
-      module Actions
-        EXIT_LOSS="EXIT_LOSS"
-        EXIT_PROFIT="EXIT_PROFIT"
-        HOLD="HOLD"
-        ADJUST="ADJUST"
-        ADJUST_PUT="ADJUST_PUT"
-        ADJUST_CALL="ADJUST_CALL"
-        ROLL_UP="ROLL_UP"
-        ROLL_DOWN="ROLL_AWAY"
-        ROLL_OUT="ROLL_OUT"
-      end
+      attr_accessor :increment, :round, :exit_threshold, :max_loss, :quantity,
+        :underlying_symbol
 
       def initialize(
         underlying_symbol: nil, increment: 0.01,
@@ -36,6 +21,7 @@ module Services
         @exit_threshold = exit_threshold
         @max_loss = max_loss
         @quantity = quantity
+        initialize_orderable
       end
 
       def credit_debit
@@ -59,13 +45,13 @@ module Services
       end
 
       def progress
-        return nil unless open_credit_debit
+        return nil unless filled_open_credit_debit
 
-        (net_credit_debit - net_open_credit_debit) / net_open_credit_debit
+        (net_credit_debit - net_filled_open_credit_debit) / net_filled_open_credit_debit
       end
 
-      def net_open_credit_debit
-        open_credit_debit * 100 - open_fees - open_commission
+      def net_filled_open_credit_debit
+        filled_open_credit_debit.to_f * 100 - filled_open_fees.to_f - filled_open_commission.to_f
       end
 
       def nearest_increment(value)
