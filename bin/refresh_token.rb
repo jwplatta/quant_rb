165d854ffb36949e2739d793430521347dab8ef3
Joseph Platta <jwplatta@gmail.com>
2025-05-07 23:07:29 -0500

    add a refresh token script

---
+++ bin/refresh_token.rb
@@ -0,0 +1,15 @@
+require "pry"
+require "dotenv"
+require "schwab_rb"
+
+Dotenv.load
+
+token_path = ENV["TOKEN_PATH"]
+puts "Token path: #{token_path}"
+
+client = SchwabRb::Auth.init_client_easy(
+  ENV["SCHWAB_API_KEY"],
+  ENV["SCHWAB_APP_SECRET"],
+  ENV["APP_CALLBACK_URL"],
+  token_path
+)
