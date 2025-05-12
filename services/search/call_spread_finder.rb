1b653f086248922a068ad7cc8239f10b436c67d5
Joseph Platta <jwplatta@gmail.com>
2025-05-11 23:37:34 -0500

    remove end_date from call spred finder and filter the opt chain directly. fix the unit tests for the spread finderes

--- services/search/call_spread_finder.rb
+++ services/search/call_spread_finder.rb
@@ -1,20 +1,16 @@
 require "pry"
-require_relative "../../mixins/schwab/schwab"
 require_relative "../trades/call_option"
 require_relative "../trades/call_spread"
 
 module Services
   module Search
     class CallSpreadFinder
-      include Schwab
-
-      attr_reader :symbol, :end_date, :short_delta, :max_spread,
+      attr_reader :symbol, :expiration_date, :short_delta, :max_spread,
         :min_credit, :min_open_interest, :dist_from_strike, :trades, :short_legs,
         :expiration_date, :quantity, :opt_chain
 
       def initialize(
         symbol:,
-        end_date: Date.today + 90,
         expiration_date: nil,
         short_delta: 0.15,
         max_spread: 20.0,
@@ -25,7 +21,6 @@ module Services
         quantity: 1
       )
         @symbol = symbol
-        @end_date = end_date
         @expiration_date = expiration_date
         @short_delta = short_delta
         @max_spread = max_spread
@@ -39,10 +34,14 @@ module Services
       end
 
       def search
-        @short_legs = opt_chain.filter(
-          put_call: :call,
-          filters: short_filters
-        )
+        short_legs = opt_chain.call_opts.select do |option|
+          option.expiration_date == expiration_date &&
+            option.mark * 100.0 >= min_credit &&
+            option.delta.abs <= short_delta &&
+            option.delta.abs >= 0.00 &&
+            option.open_interest >= min_open_interest &&
+            ((opt_chain.underlying_price - option.strike) / opt_chain.underlying_price).abs >= dist_from_strike
+        end
 
         short_legs.each do |short_raw|
           short_leg = Services::Trades::CallOption.new(
@@ -55,71 +54,35 @@ module Services
             expiration_date: short_raw.expiration_date,
             quantity: quantity
           )
-          potential_longs = opt_chain.filter(put_call: :call, filters: long_filters(short_leg))
 
-          if potential_longs.any?
-            best_long_raw = potential_longs.min_by(&:mark)
-            long_leg = Services::Trades::CallOption.new(
-              best_long_raw.symbol,
-              strike: best_long_raw.strike,
-              delta: best_long_raw.delta,
-              mark: best_long_raw.mark,
-              ask: best_long_raw.ask,
-              bid: best_long_raw.bid,
-              expiration_date: best_long_raw.expiration_date,
-              quantity: quantity
-            )
-
-            @trades << Services::Trades::CallSpread.new(
-              short_leg: short_leg,
-              long_leg: long_leg
-            )
+          potential_longs = short_legs.select do |long_raw|
+            long_raw.expiration_date == short_leg.expiration_date &&
+              ((short_leg.mark - long_raw.mark) * 100.0) >= min_credit &&
+              long_raw.strike > short_leg.strike &&
+              (long_raw.strike - short_leg.strike).abs <= max_spread
           end
+
+          next unless potential_longs.any?
+
+          best_long_raw = potential_longs.min_by(&:mark)
+          long_leg = Services::Trades::CallOption.new(
+            best_long_raw.symbol,
+            strike: best_long_raw.strike,
+            delta: best_long_raw.delta,
+            mark: best_long_raw.mark,
+            ask: best_long_raw.ask,
+            bid: best_long_raw.bid,
+            expiration_date: best_long_raw.expiration_date,
+            quantity: quantity
+          )
+
+          @trades << Services::Trades::CallSpread.new(
+            short_leg: short_leg,
+            long_leg: long_leg
+          )
         end
 
         @trades.max_by(&:credit_debit)
-      end
-
-      def short_filters
-        [
-          [
-            :delta,
-            ->(delta) { delta.abs <= short_delta && delta.abs >= 0.00 }
-          ],
-          [:open_interest, ">", min_open_interest],
-          [
-            :strike,
-            ->(strike) do
-              ((@opt_chain.underlying_price - strike) / @opt_chain.underlying_price).abs >= dist_from_strike
-            end
-          ],
-          [
-            :mark,
-            ->(mark) { mark * 100.0 >= min_credit }
-          ]
-        ].then do |filters|
-          unless expiration_date.nil?
-            filters << [:expiration_date, "==", expiration_date]
-          else
-            filters << [:expiration_date, "<=", end_date]
-          end
-        end
-      end
-
-      def long_filters(short)
-        [
-          [
-            :strike,
-            ->(strike) { (short.strike..(short.strike + max_spread.to_f)).cover? strike }
-          ],
-          [:open_interest, ">", min_open_interest],
-          [:expiration_date, "==", short.expiration_date],
-          [
-            :mark,
-            ->(mark) { (short.mark - mark) * 100.0 >= min_credit }
-          ],
-          [:delta, ->(delta) { delta.abs >= 0.00 && delta.abs <= 1.0 }],
-        ]
       end
     end
   end
