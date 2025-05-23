a2201b5dc7d82c69704add7725587bdb5daa64c5
Joseph Platta <jwplatta@gmail.com>
2025-05-22 23:20:44 -0500

    remove quote and risk logic from option classes

--- services/trades/call_option.rb
+++ services/trades/call_option.rb
@@ -6,28 +6,6 @@ module Services
   module Trades
     class CallOption < Trade
       class << self
-        def from_h(hash)
-          expiration_date = hash[:expiration_date] ? Date.parse(hash[:expiration_date]) : nil
-          CallOption.new(
-            hash[:symbol],
-            strike: hash[:strike],
-            expiration_date: expiration_date,
-            quantity: hash.fetch(:quantity, nil)
-          ).tap do |call_option|
-            # Set the filled values
-            call_option.instance_variable_set(:@filled_open_credit_debit,
-                                              hash.fetch(:filled_open_credit_debit,
-                                                         nil) || hash.fetch(:open_credit_debit, nil))
-            call_option.instance_variable_set(:@filled_open_date,
-                                              hash.fetch(:filled_open_date, nil) || hash.fetch(:open_date, nil))
-            call_option.instance_variable_set(:@filled_open_fees,
-                                              hash.fetch(:filled_open_fees, nil) || hash.fetch(:open_fees, nil))
-            call_option.instance_variable_set(:@filled_open_commission,
-                                              hash.fetch(:filled_open_commission,
-                                                         nil) || hash.fetch(:open_commission, nil))
-          end
-        end
-
         def from_quote(quote)
           expiration_date = Date.new(
             quote.expiration_year,
@@ -70,24 +48,6 @@ module Services
         @open_interest = open_interest
       end
 
-      def risk_status
-        if delta < 0.16
-          'GREEN'
-        elsif delta < 0.26
-          'YELLOW'
-        else
-          'RED'
-        end
-      end
-
-      def tested?
-        risk_status == 'YELLOW'
-      end
-
-      def danger?
-        risk_status == 'RED'
-      end
-
       def credit_debit
         nearest_increment(mark.round(2))
       end
@@ -112,36 +72,6 @@ module Services
         elsif quantity.positive?
           'BUY_TO_CLOSE'
         end
-      end
-
-      ##################
-      ### Schwab API ###
-      ##################
-
-      def check_market
-        # NOTE: from schwab mixin
-        quote(symbol).then do |q|
-          @delta = q.delta.abs
-          @mark = q.mark
-          @ask = q.ask_price
-          @bid = q.bid_price
-          @strike = q.strike_price
-          @expiration_date = Date.new(
-            q.expiration_year,
-            q.expiration_month,
-            q.expiration_day
-          )
-        end
-      end
-
-      def to_h
-        {
-          type: 'CALL',
-          symbol: symbol,
-          strike: strike,
-          quantity: quantity,
-          expiration_date: expiration_date
-        }
       end
     end
   end
