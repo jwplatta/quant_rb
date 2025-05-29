8adbdbc4f490bc12daa49f450fbb98e7f0d86c7f
Joseph Platta <jwplatta@gmail.com>
2025-05-28 23:45:39 -0500

    rename mixin and remove unused methods

--- services/trades/trade.rb
+++ services/trades/trade.rb
@@ -3,7 +3,7 @@
 require_relative '../../mixins/schwab/schwab'
 require_relative '../../mixins/orderable'
 require_relative '../../mixins/quoteable'
-require_relative '../../mixins/risk_assessable'
+require_relative '../../mixins/position_progress'
 
 Order = Struct.new(:id, :status, :date)
 
@@ -17,27 +17,14 @@ module Services
       attr_accessor :increment, :round, :exit_threshold, :max_loss, :quantity,
                     :underlying_symbol
 
-      def initialize(
-        underlying_symbol: nil, increment: 0.01,
-        round: 2, exit_threshold: 0.75, max_loss: -3.0, quantity: 1
-      )
+      def initialize(underlying_symbol: nil, increment: 0.01, round: 2, quantity: 1)
         @underlying_symbol = underlying_symbol
         @increment = increment
         @round = round
-        @exit_threshold = exit_threshold
-        @max_loss = max_loss
         @quantity = quantity
+
         initialize_orderable
-      end
-
-      def exitable?
-        progress >= exit_threshold || progress <= max_loss
-      end
-
-      def progress
-        return nil unless filled_open_credit_debit
-
-        (net_credit_debit - net_filled_open_credit_debit) / net_filled_open_credit_debit
+        init_progress
       end
 
       def net_filled_open_credit_debit
