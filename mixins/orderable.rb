686e227a274966777b84425365ee66880f3f1ab4
Joseph Platta <jwplatta@gmail.com>
2025-06-09 23:06:21 -0700

    add order preview instance vars

--- mixins/orderable.rb
+++ mixins/orderable.rb
@@ -5,16 +5,40 @@ require_relative 'schwab/schwab'
 module Orderable
   include Schwab
 
-  attr_reader :order, :order_id, :order_status, :order_rejects, :filled_order, :transactions
+  attr_reader :order, :order_id, :order_status, :order_rejects,
+    :filled_order, :transactions,
+    :order_preview, :order_preview_fees, :order_preview_commission,
+    :order_preview_status, :order_preview_rejects
 
   def initialize_orderable
     @filled_order = nil
     @order = nil
     @transactions = []
+    @order_preivew = nil
+    @order_preview_fees = nil
+    @order_preview_commission = nil
+    @order_preview_status = nil
+    @order_preview_rejects = []
+  end
+
+  def projected_net_credit_debit
+    order_preview&.price * 100 - order_preview_fees - order_preview_commission
+  end
+
+  def projected_credit_debit
+    order_preview&.price * 100
   end
 
   def order_id=(id)
     @order_id = id
+  end
+
+  def order_status
+    order&.status || @order_status || 'UNKNOWN'
+  end
+
+  def order_entered_time
+    @order&.entered_time
   end
 
   def filled_credit_debit
@@ -34,11 +58,11 @@ module Orderable
   end
 
   def filled_order_date
-    @filled_order&.entered_time&.to_date
+    @filled_order&.entered_time
   end
 
   def filled_open_date
-    @filled_order&.entered_time&.to_date
+    @filled_order&.entered_time
   end
 
   def filled_open_credit_debit
@@ -78,14 +102,17 @@ module Orderable
     %w[REJECTED EXPIRED CANCELED].include?(order_status)
   end
 
-  def preview(order_instruction: :entry)
+  def preview(order_instruction: :open)
     build_and_preview_order(
       self,
       quantity: quantity,
       order_instruction: order_instruction
     ).then do |order_preview|
-      @order_status = order_preview.order_strategy.status
-      @order_rejects = if order_preview.accepted?
+      @order_preview = order_preview
+      @order_preview_fees = order_preview.fees
+      @order_preview_commission = order_preview.commission
+      @order_preview_status = order_preview.status
+      @order_preview_rejects = if order_preview.accepted?
                          []
                        else
                          order_preview.order_validation_result.rejects(&:activity_message)
@@ -93,7 +120,7 @@ module Orderable
     end
   end
 
-  def replace(order_instruction: :entry)
+  def replace(order_instruction: :open)
     return nil unless order_id
 
     build_and_replace_order(order_id, self, quantity: quantity, order_instruction: order_instruction).then do |order|
@@ -101,27 +128,28 @@ module Orderable
         @order_id = order.order_id
         @order_status = order.status
         @order = order
+        @transactions = nil
       end
     end
   end
 
   def open
-    send(order_instruction: :entry)
+    send(order_instruction: :open)
   end
 
   def close
     send(order_instruction: :exit)
   end
 
-  def send(order_instruction: :entry)
+  def send(order_instruction: :open)
     build_and_place_order(self, quantity: quantity, order_instruction: order_instruction).then do |order|
       if order.nil?
-        @order_id = nil
         @order_status = 'REJECTED'
       else
         @order_id = order.order_id
         @order_status = order.status
         @order = order
+        @transactions = nil
       end
     end
   end
@@ -134,10 +162,7 @@ module Orderable
       @order = order
 
       if order.status == 'FILLED'
-        # Store the filled order
         @filled_order = order
-
-        # Keep the current order_id and status for reference
         @order_id = order.order_id
         @order_status = order.status
       elsif %w[REJECTED EXPIRED CANCELED].include?(order.status)
@@ -157,6 +182,7 @@ module Orderable
         @order_id = nil
         @order_status = nil
         @order = nil
+        @transactions = nil
       end
     end
   end
