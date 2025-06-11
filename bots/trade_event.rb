5ffb52beae5e31a431db6c1dc869d5613d874034
Joseph Platta <jwplatta@gmail.com>
2025-06-11 15:30:28 -0700

    add timestamp and delay to trade event

--- bots/trade_event.rb
+++ bots/trade_event.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 class TradeEvent
-  attr_reader :type, :payload
+  attr_reader :type, :delay, :payload, :timestamp, :action
 
-  def initialize(type, payload = {})
+  def initialize(type: 'action', delay: 0, action: nil, payload: {})
     @type = type
+    @action = action
+    @delay = delay
     @payload = payload
+    @timestamp = Time.now.utc
   end
 end
