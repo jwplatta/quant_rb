eaca66331397f1d73f7694a3b105a0e8ae062e9a
Joseph Platta <jwplatta@gmail.com>
2025-06-15 13:16:52 -0500

    rename spx weekly trade prototype and finish implementing the enter trade function

--- bots/spx_weekly_trade.rb
+++ bots/spx_weekly_trade.rb
@@ -6,35 +6,36 @@ require_relative '../services/trades/call_option'
 require_relative '../services/trades/put_option'
 require_relative '../mixins/schwab/schwab'
 
-class SchwabActions
+class SPXWeeklyTrade
   include Schwab
+
+  TRADE_FILE = "../tmp/spx_weekly_trade.json"
 
   def initialize
     @underlying_symbol = '$SPX'
     @option_root = 'SPXW'
+    @settlement_type = 'P' # PM settlement
   end
 
-  attr_reader :underlying_symbol
+  attr_reader :underlying_symbol, :option_root, :settlement_type
+
+  def run
+  end
 
   def enter_trade
     exp_date = next_weekday
     opt_chain = option_chain(
       underlying_symbol,
-      contract_type: 'PUT',
+      contract_type: 'CALL',
       from_date: exp_date,
-      to_date: exp_date
+      to_date: exp_date,
     )
-    # opt_chain = option_chain(
-    #   underlying_symbol,
-    #   contract_type: 'CALL',
-    #   days_to_expiration: 11
-    # )
 
     if opt_chain.nil?
       raise "No options chain found for #{underlying_symbol}"
     end
 
-    new_trade = Services::Search::PutSpreadFinder.new(
+    new_trade = Services::Search::CallSpreadFinder.new(
       symbol: underlying_symbol,
       expiration_date: exp_date,
       short_delta: 0.08,
@@ -42,7 +43,7 @@ class SchwabActions
       min_credit: 105.0,
       min_open_interest: 0,
       dist_from_strike: 0.01,
-      option_root: @option_root,
+      settlement_type: @settlement_type,
     ).search(opt_chain)
 
     if new_trade.is_a? Services::Trades::NullTrade
@@ -55,24 +56,50 @@ class SchwabActions
 
       new_trade.increment = 0.05
       new_trade.check_market
+
       new_trade.preview(order_instruction: :open)
 
-      binding.pry
+      if new_trade.order_preview_status == 'ACCEPTED'
+        # NOTE: if order preview is accepted, then open the trade
+        puts "Order preview accepted. Opening trade..."
+        new_trade.open
 
-      # while true
-      #   new_trade.check_market
-      #   puts "Trade credit: #{new_trade.credit}"
+        while true
+          if new_trade.filled?
+            puts "Trade filled: #{new_trade.short_leg.symbol} - #{new_trade.long_leg.symbol}"
+            puts "Order ID: #{new_trade.order_id}"
+            puts "Order status: #{new_trade.order_status}"
+            monitor_trade
+          elsif new_trade.market_change?
+            puts "Market conditions changed. Rechecking trade..."
+            # NOTE: cancel order and try to enter trade again
+            new_trade.cancel
+            enter_trade
+          elsif new_trade.working?
+            puts "Order is still working. Status: #{new_trade.order_status}"
+            sleep 5
+          else
+            puts "Order not filled yet. Status: #{new_trade.order_status}"
+            puts "Waiting for 5 seconds before checking again..."
+            sleep 5
+          end
+        end
+      else
+        puts "Order preview not accepted. Status: #{new_trade.order_preview_status}"
+        puts "Rejects: #{new_trade.order_preview_rejects.join(', ')}"
+        enter_trade
+      end
+    end
+  ensure
+    puts "Trade process completed."
 
-      #   new_trade.preview(order_instruction: :open)
-      #   puts "Order status: #{new_trade.order_preview_status}"
+    new_trade.cancel if new_trade
+  end
 
-      #   puts "waiting..."
-      #   sleep 5
-      # end
-
-      # r = new_trade.preview(order_instruction: :open)
-      # puts r
-      # binding.pry
+  def monitor_trade
+    while true
+      puts "monitoring trade..."
+      puts "Checking market conditions for #{underlying_symbol}..."
     end
   end
 
@@ -120,5 +147,5 @@ class SchwabActions
 end
 
 
-SchwabActions.new.enter_trade
-# SchwabActions.new.exit_trade
+SPXWeeklyTrade.new.enter_trade
+# SPXWeeklyTrade.new.exit_trade
