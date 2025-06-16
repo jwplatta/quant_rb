b61ae809a6cb333f9ed4dbe62f3f0ebb0125ede5
Joseph Platta <jwplatta@gmail.com>
2025-06-16 12:16:07 -0500

    add underyling symbol and refactor credit and debit methods on iron condor class

--- services/trades/iron_condor.rb
+++ services/trades/iron_condor.rb
@@ -5,12 +5,16 @@ require_relative 'trade'
 module Services
   module Trades
     class IronCondor
-      attr_reader :call_spread, :put_spread, :expiration_date
+      attr_reader :call_spread, :put_spread, :expiration_date, :underlying_symbol
 
-      def initialize(call_spread:, put_spread:, expiration_date:, increment: 0.01, round: 2, quantity: 1)
+      def initialize(
+        underlying_symbol:, call_spread:, put_spread:,
+        expiration_date:, increment: 0.01, round: 2, quantity: 1
+      )
         super(increment: increment, round: round, quantity: quantity)
         @put_spread = put_spread
         @call_spread = call_spread
+        @underlying_symbol = underlying_symbol
         @expiration_date = expiration_date
       end
 
@@ -23,26 +27,20 @@ module Services
       end
 
       def credit
-        credit_debit
-      end
-
-      def debit
-        credit_debit
-      end
-
-      def credit_debit
         nearest_increment(
-          put_spread.credit_debit_raw + call_spread.credit_debit_raw
+          put_spread.credit + call_spread.credit
         )
       end
 
-      def credit_debit_raw
-        put_spread.credit_debit_raw + call_spread.credit_debit_raw
+      def debit
+        nearest_increment(
+          put_spread.debit + call_spread.debit
+        )
       end
 
-      def expected_return
-        ((credit_debit * prob_of_profit) - (max_loss * (1 - prob_of_profit)))
-      end
+      # def expected_return
+      #   ((credit_debit * prob_of_profit) - (max_loss * (1 - prob_of_profit)))
+      # end
 
       def symbols
         put_spread.symbols + call_spread.symbols
