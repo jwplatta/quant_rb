007e9663aed7e9922d2156b16f1e9105cf17de6e
Joseph Platta <jwplatta@gmail.com>
2025-06-18 17:55:38 -0500

    mv vix checker to the platypi module

---
+++ lib/platypi/vix_checker.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'pry'
+require 'json'
+
+module Platypi
+
+  class VIXChecker
+    include Platypi::Schwab
+
+    module VIXThresholds
+      LOW = 12.0
+      HIGH = 20.0
+    end
+
+    module VIXStatusNames
+      LOW = 'low'
+      HIGH = 'high'
+      NORMAL = 'normal'
+    end
+
+    VIX_SYMBOL = '$VIX'
+
+    def check
+      if vix_quote.mark <= VIXThresholds::LOW
+        VIXStatusNames::LOW
+      elsif vix_quote.mark >= VIXThresholds::HIGH
+        VIXStatusNames::HIGH
+      else
+        VIXStatusNames::NORMAL
+      end
+    end
+
+    def refresh
+      @vix_quote = nil
+    end
+
+    def vix_quote
+      @vix_quote ||= quote(VIX_SYMBOL)
+    end
+  end
+end
