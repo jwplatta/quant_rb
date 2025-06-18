c15524ecd3813a33057cc95d0c2bd1342e4b5304
Joseph Platta <jwplatta@gmail.com>
2025-06-18 14:10:49 -0500

    add account data object to platypi

--- spec/schwab/data_objects/account_spec.rb
+++ spec/schwab/data_objects/account_spec.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 require 'rspec'
-require 'pry'
-require_relative '../../../../mixins/schwab/data_objects/account'
 
 RSpec.describe DataObjects::Account do
   let(:raw_data) do
