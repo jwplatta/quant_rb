ca0073dec776cf78866a96a2c9edadf703a19a20
Joseph Platta <jwplatta@gmail.com>
2025-07-17 12:37:31 -0500

    set account(s) in the configuration class

--- lib/options_trader/configuration.rb
+++ lib/options_trader/configuration.rb
@@ -1,11 +1,52 @@
 module OptionsTrader
   class Configuration
-    attr_accessor :log_level, :log_file, :log_to_stdout
+    attr_accessor :log_level, :log_file, :log_to_stdout, :default_account
+    attr_reader :accounts
 
     def initialize
       @log_level = :info
       @log_file = nil
       @log_to_stdout = true
+      @accounts = {}
+      @default_account = nil
+    end
+
+    def add_account(name, account_number)
+      @accounts[name.to_s] = account_number.to_s
+    end
+
+    def add_accounts(accounts_hash)
+      accounts_hash.each do |name, number|
+        add_account(name, number)
+      end
+    end
+
+    def load_accounts_from_env
+      ENV.select { |key, _| key.end_with?('_ACCOUNT') }.each do |key, value|
+        account_name = key.gsub('_ACCOUNT', '').downcase
+        add_account(account_name, value)
+      end
+    end
+
+    def account_number(name)
+      @accounts[name.to_s]
+    end
+
+    def account_names
+      @accounts.keys
+    end
+
+    def account_exists?(name)
+      @accounts.key?(name.to_s)
+    end
+
+    def remove_account(name)
+      @accounts.delete(name.to_s)
+    end
+
+    def clear_accounts
+      @accounts.clear
+      @default_account = nil
     end
   end
 
@@ -20,6 +61,38 @@ module OptionsTrader
 
     def logger
       OptionsTrader::Logger.logger
+    end
+
+    def add_account(name, account_number)
+      configuration.add_account(name, account_number)
+    end
+
+    def add_accounts(accounts_hash)
+      configuration.add_accounts(accounts_hash)
+    end
+
+    def load_accounts_from_env
+      configuration.load_accounts_from_env
+    end
+
+    def account_number(name)
+      configuration.account_number(name)
+    end
+
+    def account_names
+      configuration.account_names
+    end
+
+    def account_exists?(name)
+      configuration.account_exists?(name)
+    end
+
+    def default_account
+      configuration.default_account
+    end
+
+    def default_account=(name)
+      configuration.default_account = name
     end
   end
 end
