788728d73386f6a1797cf49267347415d03831b4
Joseph Platta <jwplatta@gmail.com>
2025-07-20 14:44:25 -0500

    fix reference to data objects in the schwab_rb

--- spec/search/put_spread_finder_spec.rb
+++ spec/search/put_spread_finder_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe OptionsTrader::PutSpreadFinder do
     )
   end
 
-  let(:option_chain) { OptionsTrader::Schwab::DataObjects::OptionChain.build(option_chain_data) }
+  let(:option_chain) { SchwabRb::DataObjects::OptionChain.build(option_chain_data) }
   let(:expiration_date) { Date.new(2025, 5, 20) }
 
   describe '#initialize' do
