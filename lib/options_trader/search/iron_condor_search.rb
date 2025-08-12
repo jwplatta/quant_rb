0da9f677cafdd48b8fab5f439cce93baacb2f82b
Joseph Platta <jwplatta@gmail.com>
2025-08-12 16:44:33 -0500

    add note and fix params

--- lib/options_trader/search/iron_condor_search.rb
+++ lib/options_trader/search/iron_condor_search.rb
@@ -38,14 +38,14 @@ module OptionsTrader
       opt_chain = option_chain(
         underlying_symbol,
         contract_type: 'ALL',
-        from_date: from_date || expiration_date,
-        to_date: to_date || expiration_date
+        strike_range: 'OTM',
+        to_date: to_date || expiration_date,
+        from_date: from_date || expiration_date
       )
 
       return NullStrategy.new unless opt_chain
 
       call_spreads = call_spread_search(
-        short_delta: short_delta,
         max_spread: max_spread,
         min_open_interest: min_open_interest,
         dist_from_strike: dist_from_strike
@@ -60,7 +60,6 @@ module OptionsTrader
       return NullStrategy.new if call_spreads.empty?
 
       put_spreads = put_spread_search(
-        short_delta: short_delta,
         max_spread: max_spread,
         min_open_interest: min_open_interest,
         dist_from_strike: dist_from_strike
@@ -80,6 +79,9 @@ module OptionsTrader
         (call_sprd.credit + put_sprd.credit) * 100 >= min_credit
       end
 
+      # NOTE: Selects the best combination of call and put spreads from all_combos_with_min_credit
+      # by maximizing the ratio of total credit received to the sum of the absolute deltas.
+      # This favors combinations that offer higher credit relative to risk (as measured by delta).
       call_spread, put_spread = all_combos_with_min_credit.max_by do |call_sprd, put_sprd|
         (call_sprd.credit + put_sprd.credit) / (call_sprd.delta.abs + put_sprd.delta.abs)
       end
@@ -99,7 +101,7 @@ module OptionsTrader
 
     private
 
-    def call_spread_search(short_delta:, max_spread:, min_open_interest:, dist_from_strike:)
+    def call_spread_search(max_spread:, min_open_interest:, dist_from_strike:)
       VerticalSpreadSearch.new(
         underlying_symbol: underlying_symbol,
         put_call: 'CALL',
@@ -112,7 +114,7 @@ module OptionsTrader
       )
     end
 
-    def put_spread_search(short_delta:, max_spread:, min_open_interest:, dist_from_strike:)
+    def put_spread_search(max_spread:, min_open_interest:, dist_from_strike:)
       VerticalSpreadSearch.new(
         underlying_symbol: underlying_symbol,
         put_call: 'PUT',
