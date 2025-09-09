d3d2c7a494f9533e1cd02d0754ad2029d8bb909f
Joe <jwplatta@gmail.com>
2025-09-08 23:02:06 -0500

    Historical Data Service (#58)
    * add get price history methods to the schwab markets data provider
    * add methods for historical prices at different frequencies to the schwab markets data provider

--- lib/options_trader/data_providers/schwab/markets.rb
+++ lib/options_trader/data_providers/schwab/markets.rb
@@ -52,6 +52,72 @@ module OptionsTrader
             client.get_option_chain(symbol, **kwargs)
           end
         end
+
+        def get_price_history(symbol, **kwargs)
+          validate_symbol(symbol)
+
+          kwargs = kwargs.merge({
+            need_extended_hours_data: true,
+            need_previous_close: false,
+            return_data_objects: true
+          })
+
+          handle_api_errors("get_price_history") do
+            client.get_price_history(symbol, **kwargs)
+          end
+        end
+
+        def get_price_history_every_min(symbol:, start_datetime:, end_datetime:)
+          kwargs = {
+            start_datetime: start_datetime,
+            end_datetime: end_datetime,
+            period_type: SchwabRb::PriceHistory::PeriodTypes::DAY,
+            period: SchwabRb::PriceHistory::Periods::ONE_DAY,
+            frequency_type: SchwabRb::PriceHistory::FrequencyTypes::MINUTE,
+            frequency: SchwabRb::PriceHistory::Frequencies::EVERY_MINUTE
+          }
+
+          get_price_history(symbol, **kwargs)
+        end
+
+        def get_price_history_every_five_min(symbol:, start_datetime:, end_datetime:)
+          kwargs = {
+            start_datetime: start_datetime,
+            end_datetime: end_datetime,
+            period_type: SchwabRb::PriceHistory::PeriodTypes::DAY,
+            period: SchwabRb::PriceHistory::Periods::ONE_DAY,
+            frequency_type: SchwabRb::PriceHistory::FrequencyTypes::MINUTE,
+            frequency: SchwabRb::PriceHistory::Frequencies::EVERY_FIVE_MINUTES
+          }
+
+          get_price_history(symbol, **kwargs)
+        end
+
+        def get_price_history_every_ten_min(symbol:, start_datetime:, end_datetime:)
+          kwargs = {
+            start_datetime: start_datetime,
+            end_datetime: end_datetime,
+            period_type: SchwabRb::PriceHistory::PeriodTypes::DAY,
+            period: SchwabRb::PriceHistory::Periods::ONE_DAY,
+            frequency_type: SchwabRb::PriceHistory::FrequencyTypes::MINUTE,
+            frequency: SchwabRb::PriceHistory::Frequencies::EVERY_TEN_MINUTES
+          }
+
+          get_price_history(symbol, **kwargs)
+        end
+
+        def get_price_history_everyday(symbol:, start_datetime:, end_datetime:)
+          kwargs = {
+            start_datetime: start_datetime,
+            end_datetime: end_datetime,
+            period_type: SchwabRb::PriceHistory::PeriodTypes::YEAR,
+            period: SchwabRb::PriceHistory::Periods::TWENTY_YEARS,
+            frequency_type: SchwabRb::PriceHistory::FrequencyTypes::DAILY,
+            frequency: SchwabRb::PriceHistory::Frequencies::EVERY_MINUTE
+          }
+
+          get_price_history(symbol, **kwargs)
+        end
       end
     end
   end
---
+++ lib/options_trader/services/historical_markets.rb
@@ -0,0 +1,33 @@
+module OptionsTrader
+  module Services
+    class HistoricalMarkets
+      def initialize(provider: nil)
+        @provider = provider
+      end
+
+      def get_price_history_everyday(symbol:, start_datetime:, end_datetime:)
+        @provider.get_price_history_everyday(symbol: symbol, start_datetime: start_datetime, end_datetime: end_datetime)
+      end
+
+      def get_price_history_every_min(symbol:, start_datetime:, end_datetime:)
+        @provider.get_price_history_every_min(symbol: symbol, start_datetime: start_datetime, end_datetime: end_datetime)
+      end
+
+      def get_price_history_every_five_min(symbol:, start_datetime:, end_datetime:)
+        @provider.get_price_history_every_five_min(symbol: symbol, start_datetime: start_datetime, end_datetime: end_datetime)
+      end
+
+      def get_price_history_every_ten_min(symbol:, start_datetime:, end_datetime:)
+        @provider.get_price_history_every_ten_min(symbol: symbol, start_datetime: start_datetime, end_datetime: end_datetime)
+      end
+
+      def get_price_history(symbol:, start_datetime:, end_datetime:)
+        kwargs = {
+          start_datetime: start_datetime,
+          end_datetime: end_datetime
+        }
+        @provider.get_price_history(symbol, **kwargs)
+      end
+    end
+  end
+end
