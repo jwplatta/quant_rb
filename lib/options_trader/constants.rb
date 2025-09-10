9102b5890b9d81d8098fae21e4cbc45a0c6bccdc
Joseph Platta <jwplatta@gmail.com>
2025-09-09 23:14:00 -0500

    add intervals to constants

--- lib/options_trader/constants.rb
+++ lib/options_trader/constants.rb
@@ -9,4 +9,10 @@ module OptionsTrader
     VERTICAL,
     SINGLE
   ].freeze
+  module Intervals
+    ONE_MIN = '1min'.freeze
+    FIVE_MIN = '5min'.freeze
+    TEN_MIN = '10min'.freeze
+    DAILY = 'daily'.freeze
+  end
 end
