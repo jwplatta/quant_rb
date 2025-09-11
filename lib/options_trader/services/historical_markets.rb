0bdcffccdb05157dcb56b2c9a632341539b651a7
Joseph Platta <jwplatta@gmail.com>
2025-09-10 21:24:58 -0500

    add get price hist by interval to historical markets service

--- lib/options_trader/services/historical_markets.rb
+++ lib/options_trader/services/historical_markets.rb
@@ -5,6 +5,19 @@ module OptionsTrader
         @provider = provider
       end
 
+      def get_price_history_by_interval(symbol:, start_datetime:, end_datetime:, interval:)
+        case interval
+        when OptionsTrader::Intervals::ONE_MIN
+          get_price_history_every_min(symbol: symbol, start_datetime: start_datetime, end_datetime: end_datetime)
+        when OptionsTrader::Intervals::FIVE_MIN
+          get_price_history_every_five_min(symbol: symbol, start_datetime: start_datetime, end_datetime: end_datetime)
+        when OptionsTrader::Intervals::TEN_MIN
+          get_price_history_every_ten_min(symbol: symbol, start_datetime: start_datetime, end_datetime: end_datetime)
+        when OptionsTrader::Intervals::DAILY
+          get_price_history_everyday(symbol: symbol, start_datetime: start_datetime, end_datetime: end_datetime)
+        end
+      end
+
       def get_price_history_everyday(symbol:, start_datetime:, end_datetime:)
         @provider.get_price_history_everyday(symbol: symbol, start_datetime: start_datetime, end_datetime: end_datetime)
       end
