a0dc5b734822e80788e0f101dc38593fde93c608
Joseph Platta <jwplatta@gmail.com>
2025-09-12 22:31:20 -0500

    scripts for estimating option price and delta using different volatility calculations

--- scripts/estimate_option_implied_vol.rb
+++ scripts/estimate_option_implied_vol.rb
@@ -3,16 +3,16 @@
 require_relative '../config/environment'
 require_relative '../lib/options_trader'
 
-DELTA_TOLERANCE = 0.05
-VOLATILITY_TOLERANCE = 0.12
-RISK_FREE_RATE = 0.05
+VOLATILITY_TOLERANCE = 0.1
+RISK_FREE_RATE = 0.04
 
 records = OptionsTrader::OptionChainHistory
-  .where(delta: 0.35...0.65)
+  # .where(delta: -0.55...-0.45)
+  .where(delta: 0.45...0.5)
   .where.not(underlying_price: nil, bid: nil, ask: nil)
   .order(:expiration_date, :strike, :contract_type)
 
-puts "Found #{records.count} records with delta between 0.35 and 0.65"
+puts "Found #{records.count} records."
 
 bs_large_delta_diff_cnt = 0
 bs_large_price_diff_cnt = 0
@@ -38,7 +38,9 @@ records.each do |record|
     option_type: option_type
   )
 
-  next unless implied_vol
+  next unless implied_vol && implied_vol > 0
+
+  puts "Implied Volatility: #{implied_vol.round(4)} for #{record.underlying_symbol} #{record.contract_type} #{record.expiration_date} $#{record.strike}"
 
   crr_theoretical_price = OptionsTrader::Indicators::CoxRossRubinstein.calculate(
     spot_price: record.underlying_price,
@@ -75,6 +77,8 @@ records.each do |record|
     volatility: implied_vol,
     option_type: option_type
   )
+  crr_delta = -crr_delta
+  bs_delta = -bs_delta
 
   bs_delta_diff = (bs_delta - record.delta).abs
   crr_delta_diff = (crr_delta - record.delta).abs
@@ -85,30 +89,17 @@ records.each do |record|
   puts "#{record.underlying_symbol} #{record.contract_type} #{record.expiration_date} $#{record.strike}"
   puts "  Record Delta: #{record.delta.round(4)} / CRR Delta: #{crr_delta.round(4)} / BS Delta: #{bs_delta.round(4)}"
   puts "  Record Price: $#{market_price.round(2)} / CRR Price: $#{crr_theoretical_price.round(2)} / BS Price: $#{bs_theoretical_price.round(2)}"
+  puts "----------------------------------------------------------------"
 
-  if bs_delta_diff > VOLATILITY_TOLERANCE
-    bs_large_delta_diff_cnt += 1
-    puts "  !!!BS Delta diff: #{bs_delta_diff.round(4)}"
-  end
-
-  if crr_delta_diff > VOLATILITY_TOLERANCE
-    crr_large_delta_diff_cnt += 1
-    puts "  !!!CRR Delta diff: #{crr_delta_diff.round(4)}"
-  end
-
-  if crr_price_diff > VOLATILITY_TOLERANCE
-    crr_large_price_diff_cnt += 1
-  end
-
-  if bs_price_diff > VOLATILITY_TOLERANCE
-    bs_large_price_diff_cnt += 1
-  end
+  bs_large_delta_diff_cnt += 1 if bs_delta_diff > VOLATILITY_TOLERANCE
+  crr_large_delta_diff_cnt += 1 if crr_delta_diff > VOLATILITY_TOLERANCE
+  crr_large_price_diff_cnt += 1 if crr_price_diff > VOLATILITY_TOLERANCE
+  bs_large_price_diff_cnt += 1 if bs_price_diff > VOLATILITY_TOLERANCE
 end
 
 puts "####################################################################"
-puts "Records with a large BS delta difference: #{bs_large_delta_diff_cnt}"
-puts "Records with a large CRR delta difference: #{crr_large_delta_diff_cnt}"
-puts "Records with a large BS price difference: #{bs_large_price_diff_cnt}"
-puts "Records with a large CRR price difference: #{crr_large_price_diff_cnt}"
-
-puts "Verification complete. Used CRR pricing model and implied volatility calculations."
+puts "Large BS delta difference: #{bs_large_delta_diff_cnt} - #{bs_large_delta_diff_cnt / records.count.to_f * 100}%"
+puts "Large CRR delta difference: #{crr_large_delta_diff_cnt} - #{crr_large_delta_diff_cnt / records.count.to_f * 100}%"
+puts "Large BS price difference: #{bs_large_price_diff_cnt} - #{bs_large_price_diff_cnt / records.count.to_f * 100}%"
+puts "Large CRR price difference: #{crr_large_price_diff_cnt} - #{crr_large_price_diff_cnt / records.count.to_f * 100}%"
+puts "####################################################################"
