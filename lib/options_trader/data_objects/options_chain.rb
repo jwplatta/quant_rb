4a832150cc408cc493c80e326389036a37e20db1
Joseph Platta <jwplatta@gmail.com>
2025-09-22 23:01:54 -0500

    add underlying price to options chain do

--- lib/options_trader/data_objects/options_chain.rb
+++ lib/options_trader/data_objects/options_chain.rb
@@ -1,13 +1,14 @@
 module OptionsTrader
   module DataObjects
     class OptionsChain
-      def initialize(symbol:, call_opts: [], put_opts: [])
+      def initialize(symbol:, underlying_price: nil, call_opts: [], put_opts: [])
         @symbol = symbol
+        @underlying_price = underlying_price
         @call_opts = call_opts
         @put_opts = put_opts
       end
 
-      attr_reader :symbol, :call_opts, :put_opts
+      attr_reader :symbol, :underlying_price, :call_opts, :put_opts
     end
   end
 end
