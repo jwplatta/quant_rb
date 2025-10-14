204fe3a1fc979864c13cc51569d6f7cd879df8b1
Joseph Platta <jwplatta@gmail.com>
2025-10-14 09:24:46 -0500

    index root symbol and source column

--- db/migrate/20250801232150_create_option_chain_history.rb
+++ db/migrate/20250801232150_create_option_chain_history.rb
@@ -3,7 +3,7 @@ class CreateOptionChainHistory < ActiveRecord::Migration[8.0]
     create_table :option_chain_history do |t|
       # Contract identification
       t.string :symbol, null: false, index: true
-      t.string :root_symbol
+      t.string :root_symbol, null: false, index: true
       t.string :underlying_symbol, null: false, index: true
       t.date :expiration_date, null: false, index: true
       t.decimal :strike, precision: 10, scale: 2, null: false, index: true
@@ -46,21 +46,14 @@ class CreateOptionChainHistory < ActiveRecord::Migration[8.0]
       t.decimal :open_price, precision: 10, scale: 2
       t.decimal :close_price, precision: 10, scale: 2
 
+      t.string :source, null: true, index: true
+
       # Bitemporal timestamps
       t.timestamp :valid_time, null: false, index: true
       t.timestamp :transaction_time, null: false, default: -> { 'CURRENT_TIMESTAMP' }, index: true
     end
 
-    # Composite indexes for efficient querying
-    add_index :option_chain_history, [:underlying_symbol, :expiration_date, :valid_time]
-    add_index :option_chain_history, [:symbol, :valid_time]
-    add_index :option_chain_history, [:underlying_symbol, :contract_type, :valid_time]
-    add_index :option_chain_history, [:expiration_date, :strike, :contract_type, :valid_time]
-
-    # Constraint to ensure contract_type is either PUT or CALL
     add_check_constraint :option_chain_history, "contract_type IN ('PUT', 'CALL')", name: 'valid_contract_type'
-
-    # Constraint to ensure strike is positive
     add_check_constraint :option_chain_history, 'strike > 0', name: 'positive_strike'
   end
 end
