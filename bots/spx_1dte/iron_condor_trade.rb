0ddf3376c9a31282932bef0c000d4160114563d8
Joe <jwplatta@gmail.com>
2025-11-20 21:20:03 -0600

    Feature/update the trade after the adjustment orders fill (#100)
    * remove markets from iron condor trade and add methods to set adjust details
    * assume that vertical roll orders are accepted
    * write option chain to csv file

--- bots/spx_1dte/iron_condor_trade.rb
+++ bots/spx_1dte/iron_condor_trade.rb
@@ -14,12 +14,6 @@ class IronCondorTrade
   CLOSED_STATUS = 'CLOSED'.freeze
   STATUSES = [NEW_STATUS, OPEN_STATUS, CLOSED_STATUS].freeze
 
-  # TODO: move these to the trade manager
-  OPEN_ACTION = 'OPEN'.freeze
-  ADJUST_ACTION = 'ADJUST'.freeze
-  CLOSE_ACTION = 'CLOSE'.freeze
-  ACTIONS = [OPEN_ACTION, ADJUST_ACTION, CLOSE_ACTION].freeze
-
   def initialize(
     id: nil,
     put_spread:,
@@ -40,8 +34,7 @@ class IronCondorTrade
     status: NEW_STATUS,
     price_increment: 0.05,
     trade_history: [],
-    adjustment_count: 0,
-    markets: nil
+    adjustment_count: 0
   )
     @id = id || SecureRandom.uuid().delete('-')
     @strategy_type = 'IRON_CONDOR'
@@ -64,13 +57,12 @@ class IronCondorTrade
     @price_increment = price_increment
     @trade_history = trade_history
     @adjustment_count = adjustment_count
-    @markets = markets
   end
 
   attr_reader :id, :strategy_type, :call_spread, :put_spread, :expiration_date,
     :open_price, :close_price, :exit_prof_thresh, :exit_loss_thresh, :status,
     :contracts, :trade_history, :open_fees, :open_commissions, :close_fees, :close_commissions,
-    :total_credit_debit, :total_fees, :total_commissions, :adjustment_count, :price_increment, :markets
+    :total_credit_debit, :total_fees, :total_commissions, :adjustment_count, :price_increment
 
   def symbols
     call_spread.symbols + put_spread.symbols
@@ -78,24 +70,6 @@ class IronCondorTrade
 
   def profit_loss
     total_credit_debit - total_fees - total_commissions
-  end
-
-  def check_market
-    call_short_leg = markets.get_quote(call_spread.short_leg.symbol).then do |q|
-      build_leg(q.symbol, q.strike, q.mark, q.delta, q.contract_type, q.expiration_date)
-    end
-    call_long_leg = markets.get_quote(call_spread.long_leg.symbol).then do |q|
-      build_leg(q.symbol, q.strike, q.mark, q.delta, q.contract_type, q.expiration_date)
-    end
-    @call_spread = VerticalSpread.new(call_short_leg, call_long_leg, 'CALL')
-
-    put_short_leg = markets.get_quote(put_spread.short_leg.symbol).then do |q|
-      build_leg(q.symbol, q.strike, q.mark, q.delta, q.contract_type, q.expiration_date)
-    end
-    put_long_leg = markets.get_quote(put_spread.long_leg.symbol).then do |q|
-      build_leg(q.symbol, q.strike, q.mark, q.delta, q.contract_type, q.expiration_date)
-    end
-    @put_spread = VerticalSpread.new(put_short_leg, put_long_leg, 'PUT')
   end
 
   def build_leg(symbol, strike, mark, delta, contract_type, expiration_date)
@@ -110,14 +84,14 @@ class IronCondorTrade
   end
 
   # NOTE: this is WorkingOrder object
-  def open(order)
-    fees = order.details[:fees]
-    commissions = order.details[:commissions]
-    price = order.details[:price]
-    quantity = order.details[:quantity]
-    credit_debit = calc_credit_debit(price, quantity, :credit)
+  def open(**order_dtls)
+    fees = order_dtls[:fees]
+    commissions = order_dtls[:commissions]
+    price = order_dtls[:price]
+    quantity = order_dtls[:quantity]
+    credit_debit = calc_credit_debit(price, quantity, order_dtls[:credit_debit])
 
-    update_history(OPEN_ACTION, order)
+    update_history(order_dtls)
 
     @status = OPEN_STATUS
     @open_price = price
@@ -130,37 +104,57 @@ class IronCondorTrade
     trades_file_manager.create(self)
   end
 
-  def adjust(order)
-    # TODO: need to update the spreads if the contracts changed.
-    fees = order.details[:fees]
-    commissions = order.details[:commissions]
-    price = order.details[:price]
-    quantity = order.details[:quantity]
-    credit_debit = calc_credit_debit(price, quantity, :credit)
+  def adjust_call_spread(new_spread, **order_dtls)
+    fees = order_dtls[:fees]
+    commissions = order_dtls[:commissions]
+    price = order_dtls[:price]
+    quantity = order_dtls[:quantity]
+    credit_debit = calc_credit_debit(price, quantity, order_dtls[:credit_debit])
 
-    update_history(order)
+    update_history(order_dtls)
 
     @adjustment_count += 1
+
     @total_credit_debit += credit_debit
     @total_fees += fees if fees
     @total_commissions += commissions if commissions
+    @call_spread = new_spread
 
     trades_file_manager.update(self)
   end
 
-  def close(order)
-    fees = order.details[:fees]
-    commissions = order.details[:commissions]
-    price = order.details[:price]
-    quantity = order.details[:quantity]
-    credit_debit = calc_credit_debit(price, quantity, :debit)
+  def adjust_put_spread(new_spread, **order_dtls)
+    fees = order_dtls[:fees]
+    commissions = order_dtls[:commissions]
+    price = order_dtls[:price]
+    quantity = order_dtls[:quantity]
+    credit_debit = calc_credit_debit(price, quantity, order_dtls[:credit_debit])
 
-    update_history(CLOSE_ACTION, order)
+    update_history(order_dtls)
+
+    @adjustment_count += 1
+
+    @total_credit_debit += credit_debit
+    @total_fees += fees if fees
+    @total_commissions += commissions if commissions
+    @put_spread = new_spread
+
+    trades_file_manager.update(self)
+  end
+
+  def close(**order_dtls)
+    fees = order_dtls[:fees]
+    commissions = order_dtls[:commissions]
+    price = order_dtls[:price]
+    quantity = order_dtls[:quantity]
+    credit_debit = calc_credit_debit(price, quantity, order_dtls[:credit_debit])
+    update_history(order_dtls)
 
     @status = CLOSED_STATUS
     @close_price = price
     @close_fees = fees
     @close_commissions = commissions
+
     @total_credit_debit += credit_debit
     @total_fees += fees if fees
     @total_commissions += commissions if commissions
@@ -176,10 +170,9 @@ class IronCondorTrade
     end
   end
 
-  def update_history(action, order)
-    trade_event = order.details.dup
+  def update_history(kwargs)
+    trade_event = kwargs.dup
     trade_event[:timestamp] = Time.now.utc.iso8601
-
     @trade_history << trade_event
   end
 
