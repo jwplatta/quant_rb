eb3e8a43a49d0614355e9290fe03355c49a7f3af
Joseph Platta <jwplatta@gmail.com>
2025-11-21 20:00:26 -0600

    extract bot class to its own file

---
+++ bots/spx_1dte/bot.rb
@@ -0,0 +1,120 @@
+# frozen_string_literal: true
+
+require 'date'
+require_relative './iron_condor_trade'
+
+class SPX1DTEBot
+  # NOTE: technically not the market open and close, but want to start monitoring before
+  # the market open and during the extended hours
+  MARKET_OPEN = "08:25 AM"
+  MARKET_CLOSE = "03:15 PM"
+  TRADE_WINDOW_START = "02:59 PM"
+  TRADE_WINDOW_END = "03:15 PM"
+
+  def initialize(
+    trade_finder:, trade_state_machine:, order_manager:, logger:
+  )
+    @trade_finder = trade_finder
+    @trade_state_machine = trade_state_machine
+    @order_manager = order_manager
+    @logger = logger
+    @trade = nil
+  end
+
+  attr_reader :trade_finder, :trade_state_machine, :order_manager, :trade, :logger
+
+  def run
+    while true
+      @trade = if outside_market_hours?
+        sleep_interval = seconds_until_market_open
+        @logger.info "Outside market hours. Sleeping for #{sleep_interval / 60} minutes."
+        sleep(sleep_interval)
+        nil
+      elsif !open_trade.nil?
+        @logger.info "Continuing to manage existing open trade #{open_trade.id}."
+        open_trade
+      elsif inside_trade_window?
+        new_trade = trade_finder.search
+        send_order(new_trade) if new_trade.status == 'NEW'
+      else
+        sleep_interval = seconds_until_trade_window_start
+        @logger.info "No trade to manage and outside trade window. Sleep #{sleep_interval / 60} minute."
+        sleep(sleep_interval)
+        nil
+      end
+
+      next if @trade.nil?
+
+      @trade_state_machine.watch(@trade)
+      @trade_state_machine.reset
+      @logger.info "Trade #{@trade.id} closed. Resetting trade."
+    end
+  end
+
+  def open_trade
+    IronCondorTrade.open_trade
+  end
+
+  def seconds_until_trade_window_start
+    now = Time.now
+    start_time = Time.parse(TRADE_WINDOW_START)
+    start_time += 24 * 60 * 60 if start_time <= now
+    (start_time - now).to_i
+  end
+
+  def seconds_until_market_open
+    now = Time.now
+    market_open_time = Time.parse("#{Date.today} #{MARKET_OPEN}")
+    market_close_time = Time.parse("#{Date.today} #{MARKET_CLOSE}")
+
+    if now <= market_open_time
+      (market_open_time - now).to_i
+    elsif now >= market_close_time
+      market_open_time += 24 * 60 * 60
+
+      if market_open_time.wday == 6
+        market_open_time += 2 * 24 * 60 * 60
+      elsif market_open_time.wday == 0
+        market_open_time += 24 * 60 * 60
+      end
+
+      (market_open_time - now).to_i
+    end
+  end
+
+  def outside_market_hours?
+    curr_time = Time.now
+    (curr_time.hour <= 8 && curr_time.min < 25) || (curr_time.hour >= 15 && curr_time.min > 15)
+  end
+
+  def inside_trade_window?
+    curr_time = Time.now
+    curr_time >= Time.parse(TRADE_WINDOW_START) && curr_time <= Time.parse(TRADE_WINDOW_END)
+  end
+
+  def send_order(trade)
+    logger.info "Sending open order for trade #{trade.id}"
+
+    order = order_manager.open_iron_condor(trade)
+
+    if order.status == 'REJECTED'
+      # TODO: if the error can be handled, then try to handle it in the code and send the order again.
+      # Otherwise pause and try to find a new trade.
+      raise "Open order was rejected!"
+    elsif order.status == 'WORKING'
+      while order.status == 'WORKING'
+        order = order_manager.check_order_status(order.id)
+        if order.status == 'FILLED'
+          trade.open(**order.details)
+          logger.info "Order filled for trade #{trade.id}"
+          return trade
+        end
+
+        logger.info "Order status: #{order.status}. Checking again in 5 seconds."
+        sleep(5)
+      end
+    else
+      raise "Unexpected order status: #{order.status}"
+    end
+  end
+end
