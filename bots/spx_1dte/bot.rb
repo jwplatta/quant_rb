71025ec4fb13d07aa34336730a12d5e72882e4e4
Joseph Platta <jwplatta@gmail.com>
2025-12-03 19:53:51 -0600

    remove constants and use values from config in bot

--- bots/spx_1dte/bot.rb
+++ bots/spx_1dte/bot.rb
@@ -1,17 +1,11 @@
 # frozen_string_literal: true
 
 require 'date'
+require 'time'
 require_relative './iron_condor_trade'
 require_relative 'util'
 
 class SPX1DTEBot
-  # NOTE: technically not the market open and close, but want to start monitoring before
-  # the market open and during the extended hours
-  MARKET_OPEN = "08:25 AM"
-  MARKET_CLOSE = "03:15 PM"
-  TRADE_WINDOW_START = "02:59 PM"
-  TRADE_WINDOW_END = "03:15 PM"
-
   def initialize(
     config:,
     trade_finder:,
@@ -38,18 +32,22 @@ class SPX1DTEBot
       elsif open_trade
         trade = open_trade
         log "Watching open trade #{trade.id}."
-        watch_trade(trade)
+        manage_trade(trade)
       elsif inside_trade_window?
-        next_exp_date = next_vaild_expiration_date
+        next_exp_date = next_trading_day(Date.today + 1)
 
-        if safe_expiration_date?(next_exp_date)
+        # NOTE: we want to check the expiration date after finding the next next trading day
+        # because we do not want to trade over high risk dates by skipping to the next expiration date.
+        if low_risk_date?(next_exp_date)
           new_trade = trade_finder.search(expiration_date: next_exp_date)
           send_order(new_trade) if new_trade.status == 'NEW'
         else
-          log "Next expiration date #{next_exp_date} is not safe. Skipping trade."
+          sleep_interval = seconds_until_trade_window_start(Date.today + 1)
+          log "Skip trade. Next expiration date #{next_exp_date} is not safe. Sleep #{sleep_interval / 60} minutes."
+          sleep(sleep_interval)
         end
       else
-        sleep_interval = seconds_until_trade_window_start
+        sleep_interval = seconds_until_trade_window_start(Date.today)
         log "No trade to manage and outside trade window. Sleep #{sleep_interval / 60} minutes."
         sleep(sleep_interval)
       end
@@ -64,40 +62,33 @@ class SPX1DTEBot
     IronCondorTrade.open_trade
   end
 
-  def next_vaild_expiration_date
-    # TODO: get rid of the while loop
-    exp_date = Date.today + 1
-    while true
-      if exp_date.saturday?
-        exp_date += 2
-      elsif exp_date.sunday?
-        exp_date += 1
-      elsif holiday?(exp_date)
-        exp_date += 1
-      else
-        break
-      end
-    end
-
-    exp_date
+  def manage_trade(trade)
+    config.monitoring_window(Date.today)
+    @trade_state_machine.set_monitoring_window(
+      config.monitoring_start_time(Date.today),
+      config.monitoring_end_time(Date.today),
+      config.exit_by_time(Date.today)
+    )
+    @trade_state_machine.manage(trade)
   end
 
-  def watch_trade(trade)
-    @trade_state_machine.watch(trade)
-    @logger.info "Trade #{trade.id} closed. Resetting trade."
-  end
-
-  def seconds_until_trade_window_start
+  def seconds_until_trade_window_start(date)
     now = Time.now
-    start_time = Time.parse(TRADE_WINDOW_START)
-    start_time += 24 * 60 * 60 if start_time <= now
-    (start_time - now).to_i
+    start_time = config.enter_trade_window_start_time(date)
+
+    if start_time >= now
+      0
+    else
+      start_time += 86_400
+      (start_time - now).to_i
+    end
   end
 
   def seconds_until_market_open
     now = Time.now
-    market_open_time = Time.parse("#{Date.today} #{MARKET_OPEN}")
-    market_close_time = Time.parse("#{Date.today} #{MARKET_CLOSE}")
+    today = Date.today
+    market_open_time = config.monitoring_start_time(today)
+    market_close_time = config.monitoring_end_time(today)
 
     if now <= market_open_time
       (market_open_time - now).to_i
@@ -118,18 +109,43 @@ class SPX1DTEBot
     config.holiday?(date)
   end
 
-  def safe_expiration_date?(date)
-    config.safe_expiration_date?(date)
+  def low_risk_date?(date)
+    config.low_risk_date?(date)
   end
 
   def outside_market_hours?
-    curr_time = Time.now
-    (curr_time.hour <= 8 && curr_time.min < 25) || (curr_time.hour >= 15 && curr_time.min > 15)
+    now = Time.now
+    today = Date.today
+
+    return true unless trading_day?(today)
+
+    market_open_time = config.monitoring_start_time(today)
+    market_close_time = config.monitoring_end_time(today)
+
+    return true if now < market_open_time
+    return false if now <= market_close_time
+
+    next_trading_date = next_trading_day(today + 1)
+    next_open_time = config.monitoring_start_time(next_trading_date)
+
+    now < next_open_time
+  end
+
+  def next_trading_day(date)
+    next_day = date
+    next_day += 1 until trading_day?(next_day)
+    next_day
+  end
+
+  def trading_day?(date)
+    !(date.saturday? || date.sunday? || holiday?(date))
   end
 
   def inside_trade_window?
+    trade_window_start = config.enter_trade_window_start_time(Date.today)
+    trade_window_end = config.enter_trade_window_end_time(Date.today)
     curr_time = Time.now
-    curr_time >= Time.parse(TRADE_WINDOW_START) && curr_time <= Time.parse(TRADE_WINDOW_END)
+    curr_time >= trade_window_start && curr_time <= trade_window_end
   end
 
   def send_order(trade)
