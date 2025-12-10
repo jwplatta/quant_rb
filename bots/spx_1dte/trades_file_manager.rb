eaf78d3a605fdfca3c63cd2c031abe7ce9bbc407
Joseph Platta <jwplatta@gmail.com>
2025-12-09 21:36:12 -0600

    remove strategy

--- bots/spx_1dte/trades_file_manager.rb
+++ bots/spx_1dte/trades_file_manager.rb
@@ -107,7 +107,6 @@ class TradesFileManager
   end
 
   def to_trade_object(trade_data)
-    # Parse trade history events
     trade_history = if trade_data[:trade_history]
       trade_data[:trade_history].map do |e|
         e.merge({
@@ -119,73 +118,13 @@ class TradesFileManager
       []
     end
 
-    # Reconstruct init_strategy from trade history or stored data
-    init_strategy = if trade_data[:call_spread] && trade_data[:put_spread]
-      build_iron_condor_strategy(trade_data)
-    else
-      nil
-    end
-
     Trade.new(
       id: trade_data[:id],
-      init_strategy: init_strategy,
       status: trade_data[:status],
-      exit_prof_thresh: trade_data[:exit_prof_thresh],
-      exit_loss_thresh: trade_data[:exit_loss_thresh],
+      exit_prof_price: trade_data[:exit_prof_price],
+      exit_loss_mult: trade_data[:exit_loss_mult],
       price_increment: trade_data[:price_increment],
       trade_history: trade_history
-    )
-  end
-
-  private
-
-  def build_iron_condor_strategy(trade_data)
-    call_spread = build_vertical_spread(trade_data[:call_spread], 'CALL')
-    put_spread = build_vertical_spread(trade_data[:put_spread], 'PUT')
-
-    expiration_date = if trade_data[:expiration_date].is_a?(String)
-      Date.parse(trade_data[:expiration_date])
-    else
-      trade_data[:expiration_date]
-    end
-
-    IronCondor.new(
-      put_spread: put_spread,
-      call_spread: call_spread,
-      quantity: trade_data[:contracts] || 1,
-      expiration_date: expiration_date,
-      price_increment: trade_data[:price_increment] || 0.05
-    )
-  end
-
-  def build_vertical_spread(spread_data, contract_type)
-    short_leg = build_option_leg(spread_data[:short_leg], contract_type)
-    long_leg = build_option_leg(spread_data[:long_leg], contract_type)
-
-    VerticalSpread.new(
-      short_leg: short_leg,
-      long_leg: long_leg,
-      contract_type: contract_type,
-      quantity: spread_data[:contracts] || 1,
-      expiration_date: Date.parse(spread_data[:short_leg][:expiration_date])
-    )
-  end
-
-  def build_option_leg(leg_data, contract_type)
-    expiration_date = if leg_data[:expiration_date].is_a?(String)
-      Date.parse(leg_data[:expiration_date])
-    else
-      leg_data[:expiration_date]
-    end
-
-    OptionLeg.new(
-      symbol: leg_data[:symbol],
-      contract_type: contract_type,
-      strike: leg_data[:strike],
-      mark: leg_data[:mark],
-      delta: leg_data[:delta],
-      expiration_date: expiration_date,
-      quantity: 1
     )
   end
 end
