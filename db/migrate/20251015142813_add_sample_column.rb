df745e71c451b52156824e7cce3a6f918a4c1d75
Joe <jwplatta@gmail.com>
2026-04-13 15:31:02 -0500

    remove unused (#128)

--- db/migrate/20251015142813_add_sample_column.rb
+++
@@ -1,9 +0,0 @@
-class AddSampleColumn < ActiveRecord::Migration[8.0]
-  def up
-    add_column :option_chain_history, :sample, :boolean, default: false
-  end
-
-  def down
-    remove_column :option_chain_history, :sample
-  end
-end
--- db/migrate/20251015143333_add_index_on_sample_column.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexOnSampleColumn < ActiveRecord::Migration[8.0]
-  def up
-    add_index :option_chain_history, :sample
-  end
-
-  def down
-    remove_index :option_chain_history, :sample
-  end
-end
